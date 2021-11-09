7acd3b537f032b248b6d2d5f69a7986ca31c6b77
Jakub Witczak <jakubwitczak6@gmail.com>
2021-11-09 14:00:02 +0100

    Delete walletconnectV2/src/main/kotlin/org/walletconnect/walletconnectv2 directory
    Delete incorrectly named directory

--- walletconnectV2/src/main/kotlin/org/walletconnect/walletconnectv2/clientsync/pairing/before/proposal/PairingProposer.kt
+++
@@ -1,3 +0,0 @@
-package org.walletconnect.walletconnectv2.clientsync.pairing.before.proposal
-
-data class PairingProposer(val publicKey: String, val controller: Boolean)
--- walletconnectV2/src/main/kotlin/org/walletconnect/walletconnectv2/clientsync/pairing/before/proposal/PairingSignalParams.kt
+++
@@ -1,4 +0,0 @@
-package org.walletconnect.walletconnectv2.clientsync.pairing.before.proposal
-
-
-data class PairingSignalParams(val uri: String)
