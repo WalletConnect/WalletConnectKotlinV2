c6f8a8812197c92714ce49ceab02a1caf922fbea
Talha Ali <TAli@loc.gov>
2021-11-12 19:49:01 -0600

    Added ActivityScenarioRule for integration test

--- walletconnectV2/src/androidTest/kotlin/org/walletconnect/walletconnectv2/PairingIntegrationAndroidTest.kt
+++ walletconnectV2/src/androidTest/kotlin/org/walletconnect/walletconnectv2/PairingIntegrationAndroidTest.kt
@@ -13,17 +13,12 @@ import java.util.concurrent.CountDownLatch
 
 class PairingIntegrationAndroidTest {
     @get:Rule
-    val activityRule = activityScenarioRule<IntegrationTestActivity>()
+    val activityRule = wcActivityScenarioRule()
     private val app = ApplicationProvider.getApplicationContext<IntegrationTestApplication>()
-
-    @After
-    fun tearDown() {
-        activityRule.scenario.close()
-    }
 
     @Test
     fun pairing() {
-        runWCClientTest { latch ->
+        activityRule.launch {
             val initParams = ClientTypes.InitialParams(application = app, hostName = "relay.walletconnect.org")
             WalletConnectClient.initialize(initParams)
 
@@ -33,7 +28,7 @@ class PairingIntegrationAndroidTest {
             val listener = object: WalletConnectClientListener {
                 override fun onSessionProposal(proposal: WalletConnectClientData.SessionProposal) {
                     assert(true)
-                    latch.countDown()
+                    activityRule.close()
                 }
 
                 override fun onSettledSession(session: WalletConnectClientData.SettledSession) {}
@@ -49,12 +44,12 @@ class PairingIntegrationAndroidTest {
 
     @Test
     fun approve() {
-        runWCClientTest { latch ->
+        activityRule.launch {
             val initParams = ClientTypes.InitialParams(application = app, hostName = "relay.walletconnect.org")
             WalletConnectClient.initialize(initParams)
 
             val uri =
-                "wc:6fbac9a32042957b0791128874655ac5c2db64016c293b29e77484fe5d868d6a@2?controller=false&publicKey=23010bc280c5fadada0fd93f012e2c1aa0797bcf348b15a74d56b56d3ddec700&relay=%7B%22protocol%22%3A%22waku%22%7D"
+                "wc:970b0fa8367670e0411a95b27c8bd30a13b26f7f9173fb8111d581d53f03fd45@2?controller=false&publicKey=2130cf44d346b571615284ffae0c97195ca45464b18d8ff459105d84fe3aa70e&relay=%7B%22protocol%22%3A%22waku%22%7D"
             val pairingParams = ClientTypes.PairParams(uri)
             val listener = object: WalletConnectClientListener {
                 override fun onSessionProposal(proposal: WalletConnectClientData.SessionProposal) {
@@ -68,7 +63,7 @@ class PairingIntegrationAndroidTest {
 
                 override fun onSettledSession(session: WalletConnectClientData.SettledSession) {
                     assert(true)
-                    latch.countDown()
+                    activityRule.close()
                 }
 
                 override fun onSessionRequest(request: WalletConnectClientData.SessionRequest) {}
@@ -77,23 +72,6 @@ class PairingIntegrationAndroidTest {
             }
 
             WalletConnectClient.pair(pairingParams, listener)
-        }
-    }
-
-    private fun runWCClientTest(block: (CountDownLatch) -> Unit) {
-        activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
-        assert(activityRule.scenario.state.isAtLeast(Lifecycle.State.RESUMED))
-
-        activityRule.scenario.onActivity {
-            val latch = CountDownLatch(1)
-
-            block(latch)
-
-            try {
-                latch.await()
-            } catch (exception: InterruptedException) {
-                Assert.fail()
-            }
         }
     }
 }
