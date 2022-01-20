fdb1924fcd94a7590c0bb49c36bd72e09d981777
Talha Ali <TAli@loc.gov>
2022-01-20 09:56:23 -0600

    addressed PR comments

--- sample/src/main/java/com/walletconnect/sample/SampleApplication.kt
+++ sample/src/main/java/com/walletconnect/sample/SampleApplication.kt
@@ -15,16 +15,16 @@ class SampleApplication : Application() {
             useTls = true,
             hostName = WALLET_CONNECT_URL,
             projectId = PROJECT_ID,
-            isController = true,
+            isController = false,
             metadata = WalletConnect.Model.AppMetaData(
-                name = "Kotlin Wallet",
-                description = "Wallet description",
-                url = "example.wallet",
+                name = "Kotlin Dapp",
+                description = "Dapp description",
+                url = "example.dapp",
                 icons = listOf("https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media")
             )
         )
 
-        // TODO: Move to wallet example once separate workflow is added
+        // TODO: Move to Wallet example once separate workflow is added
         val init2 = WalletConnect.Params.Init(
             application = this,
             relayServerUrl = "wss://$WALLET_CONNECT_URL/?projectId=$PROJECT_ID",
@@ -48,6 +48,6 @@ class SampleApplication : Application() {
 
     private companion object {
         const val WALLET_CONNECT_URL = "relay.walletconnect.com"
-        const val PROJECT_ID = "2ee94aca5d98e6c05c38bce02bee952a"
+        const val PROJECT_ID = "" //TODO: register at https://walletconnect.com/register to get a project ID
     }
 }
