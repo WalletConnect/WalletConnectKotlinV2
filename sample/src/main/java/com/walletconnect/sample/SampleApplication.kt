ed4b578dc728f2c06002e6dddc5ccfd7dd9cc52d
kubel <jakubwitczak6@gmail.com>
2022-01-31 14:02:32 +0100

    Final clean up

--- sample/src/main/java/com/walletconnect/sample/SampleApplication.kt
+++ sample/src/main/java/com/walletconnect/sample/SampleApplication.kt
@@ -48,7 +48,6 @@ class SampleApplication : Application() {
 
     private companion object {
         const val WALLET_CONNECT_URL = "relay.walletconnect.com"
-        const val PROJECT_ID =
-            "2ee94aca5d98e6c05c38bce02bee952a-dfsfafhjdsf" //TODO: register at https://walletconnect.com/register to get a project ID
+        const val PROJECT_ID = "" //TODO: register at https://walletconnect.com/register to get a project ID
     }
 }
