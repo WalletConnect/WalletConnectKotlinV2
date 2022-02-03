1d80fd3147f828c458a64bef62349cf3a2df7b5f
Talha Ali <TAli@loc.gov>
2022-02-03 00:32:11 -0600

    merged in develop

--- walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/client/WalletConnectClient.kt
+++ walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/client/WalletConnectClient.kt
@@ -14,7 +14,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.KoinApplication
 
 object WalletConnectClient {
-
     private val wcKoinApp: KoinApplication = KoinApplication.init()
     private lateinit var engineInteractor: EngineInteractor
 
