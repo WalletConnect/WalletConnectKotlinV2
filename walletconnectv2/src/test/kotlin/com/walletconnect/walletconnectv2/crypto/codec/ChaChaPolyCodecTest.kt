28cca3735b381cefbe2f4a3984f78151107e3208
kubel <jakubwitczak6@gmail.com>
2022-04-01 12:57:30 +0200

    Provide ChaChaPoly encryption with the same nonce

--- walletconnectv2/src/test/kotlin/com/walletconnect/walletconnectv2/crypto/codec/ChaChaPolyCodecTest.kt
+++ walletconnectv2/src/test/kotlin/com/walletconnect/walletconnectv2/crypto/codec/ChaChaPolyCodecTest.kt
@@ -1,13 +1,13 @@
 package com.walletconnect.walletconnectv2.crypto.codec
 
 import com.walletconnect.walletconnectv2.core.model.vo.SecretKey
-import com.walletconnect.walletconnectv2.relay.data.codec.AuthenticatedEncryptionCodec
+import com.walletconnect.walletconnectv2.relay.data.codec.ChaChaPolyCodec
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class AuthenticatedEncryptionCodecTest {
+class ChaChaPolyCodecTest {
 
-    private val codec: AuthenticatedEncryptionCodec = AuthenticatedEncryptionCodec()
+    private val codec: ChaChaPolyCodec = ChaChaPolyCodec()
 
     @Test
     fun `ChaCha20-Poly1305 encryption test`() {
@@ -16,22 +16,16 @@ class AuthenticatedEncryptionCodecTest {
 
         val encryptedMessage = codec.encrypt(message, sharedKey)
         val text = codec.decrypt(encryptedMessage, sharedKey)
-        assertEquals(text, message)
-    }
 
-    @Test
-    fun `encrypt test`() {
-        val sharedKey = SecretKey("0653ca620c7b4990392e1c53c4a51c14a2840cd20f0f1524cf435b17b6fe988c")
-        val message = "WalletConnect"
-        val encryptedMessage = codec.encrypt(message, sharedKey)
-        assertEquals("5JJK04yH8m/DmHWxvEQ7u09nkK1IxOJqlTJ1CqQ=", encryptedMessage)
+        assertEquals(text, message)
     }
 
     @Test
     fun `decrypt test`() {
         val sharedKey = SecretKey("0653ca620c7b4990392e1c53c4a51c14a2840cd20f0f1524cf435b17b6fe988c")
-        val cipherText = "5JJK04yH8m/DmHWxvEQ7u09nkK1IxOJqlTJ1CqQ="
+        val cipherText = "cXdlY2ZhYXNkYWRzVhkbjHqli8hN0rFbAtMPIsJho4zLvWskMTQKSGw="
         val encryptedMessage = codec.decrypt(cipherText, sharedKey)
+
         assertEquals("WalletConnect", encryptedMessage)
     }
 }
