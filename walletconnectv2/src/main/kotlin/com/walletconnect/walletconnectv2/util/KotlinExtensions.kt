df3df1bdc50cf1d842ad78d409c98042a3d2dbb4
szymon <sz.rzad@gmail.com>
2022-04-14 14:24:57 +0200

    Feature/Added WalletConnectRelayer tests

--- walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/util/KotlinExtensions.kt
+++ walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/util/KotlinExtensions.kt
@@ -7,3 +7,6 @@ internal val String.Companion.Empty get() = ""
 
 @get:JvmSynthetic
 internal val Int.Companion.DefaultId get() = -1
+
+@get:JvmSynthetic
+internal val Long.Companion.DefaultId get() = -1L
