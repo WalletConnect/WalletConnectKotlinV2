d623d9fae7a893a396a4a70d166556c39caa5bc0
Talha Ali <TAli@loc.gov>
2022-04-27 23:33:38 -0500

    Removed unused imports

--- wallet/src/main/kotlin/com/walletconnect/wallet/ui/accounts/AccountsFragment.kt
+++ wallet/src/main/kotlin/com/walletconnect/wallet/ui/accounts/AccountsFragment.kt
@@ -2,7 +2,6 @@ package com.walletconnect.wallet.ui.accounts
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
