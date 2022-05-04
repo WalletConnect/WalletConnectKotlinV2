06fc1f28b967050bf037a637555f07d21e90b5a1
szymon <sz.rzad@gmail.com>
2022-05-04 12:44:27 +0200

    Exposed Relay without exposing internal VO and DO classes

--- walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/client/WalletConnect.kt
+++ walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/client/WalletConnect.kt
@@ -152,6 +152,159 @@ object WalletConnect {
             val chainId: String?,
             val params: String,
         ) : Model()
+
+        sealed class Relay : Model() {
+            sealed class DTO : Relay() {
+                abstract val id: Long
+                abstract val jsonrpc: String
+
+                sealed class Publish : DTO() {
+
+                    data class Request(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val method: String = "waku_publish",
+                        val params: Params
+                    ) : Publish() {
+
+                        data class Params(
+                            val topic: String,
+                            val message: String,
+                            val ttl: Long,
+                            val prompt: Boolean?
+                        )
+                    }
+
+                    data class Acknowledgement(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val result: Boolean
+                    ) : Publish()
+
+                    data class JsonRpcError(
+                        override val jsonrpc: String = "2.0",
+                        val error: Error,
+                        override val id: Long
+                    ) : Publish()
+                }
+
+                sealed class Subscribe : DTO() {
+
+                    data class Request(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val method: String = "waku_subscribe",
+                        val params: Params
+                    ) : Subscribe() {
+
+                        data class Params(
+                            val topic: String
+                        )
+                    }
+
+                    data class Acknowledgement(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val result: String
+                    ) : Subscribe()
+
+                    data class JsonRpcError(
+                        override val jsonrpc: String = "2.0",
+                        val error: Error,
+                        override val id: Long
+                    ) : Subscribe()
+                }
+
+                sealed class Subscription : DTO() {
+
+                    data class Request(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val method: String = "waku_subscription",
+                        val params: Params
+                    ) : Subscription() {
+
+                        val subscriptionTopic: String = params.subscriptionData.topic
+                        val message: String = params.subscriptionData.message
+
+                        data class Params(
+                            val subscriptionId: String,
+                            val subscriptionData: SubscriptionData
+                        ) {
+
+                            data class SubscriptionData(
+                                val topic: String,
+                                val message: String
+                            )
+                        }
+                    }
+
+                    data class Acknowledgement(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val result: Boolean
+                    ) : Subscription()
+
+                    data class JsonRpcError(
+                        override val jsonrpc: String = "2.0",
+                        val error: Error,
+                        override val id: Long
+                    ) : Subscription()
+                }
+
+                sealed class Unsubscribe : DTO() {
+
+                    data class Request(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val method: String = "waku_unsubscribe",
+                        val params: Params
+                    ) : Unsubscribe() {
+
+                        data class Params(
+                            val topic: String,
+                            val subscriptionId: String
+                        )
+                    }
+
+                    data class Acknowledgement(
+                        override val id: Long,
+                        override val jsonrpc: String = "2.0",
+                        val result: Boolean
+                    ) : Unsubscribe()
+
+                    data class JsonRpcError(
+                        override val jsonrpc: String = "2.0",
+                        val error: Error,
+                        override val id: Long
+                    ) : Unsubscribe()
+                }
+
+                data class Error(
+                    val code: Long,
+                    val message: String,
+                ) {
+                    val errorMessage: String = "Error code: $code; Error message: $message"
+                }
+            }
+
+            sealed class Event : Model() {
+                data class OnConnectionOpened<out WEB_SOCKET : Any>(val webSocket: WEB_SOCKET) :
+                    Event()
+
+                data class OnMessageReceived(val message: Message) : Event()
+                data class OnConnectionClosing(val shutdownReason: ShutdownReason) : Event()
+                data class OnConnectionClosed(val shutdownReason: ShutdownReason) : Event()
+                data class OnConnectionFailed(val throwable: Throwable) : Event()
+            }
+
+            sealed class Message : Model() {
+                data class Text(val value: String) : Message()
+                class Bytes(val value: ByteArray) : Message()
+            }
+
+            data class ShutdownReason(val code: Int, val reason: String) : Model()
+        }
     }
 
     sealed class Params {
--- walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/network/data/repository/RelayClient.kt
+++ walletconnectv2/src/main/kotlin/com/walletconnect/walletconnectv2/network/data/repository/RelayClient.kt
@@ -1,13 +1,15 @@
 package com.walletconnect.walletconnectv2.network.data.repository
 
+import com.walletconnect.walletconnectv2.client.WalletConnect
 import com.walletconnect.walletconnectv2.core.model.vo.SubscriptionIdVO
 import com.walletconnect.walletconnectv2.core.model.vo.TopicVO
 import com.walletconnect.walletconnectv2.core.scope.scope
-import com.walletconnect.walletconnectv2.network.model.RelayEvent
 import com.walletconnect.walletconnectv2.network.Relay
 import com.walletconnect.walletconnectv2.network.data.service.RelayService
 import com.walletconnect.walletconnectv2.network.model.RelayDTO
+import com.walletconnect.walletconnectv2.network.model.toRelayAcknowledgment
 import com.walletconnect.walletconnectv2.network.model.toRelayEvent
+import com.walletconnect.walletconnectv2.network.model.toRelayRequest
 import com.walletconnect.walletconnectv2.util.Logger
 import com.walletconnect.walletconnectv2.util.generateId
 import kotlinx.coroutines.cancel
@@ -17,36 +19,38 @@ import kotlinx.coroutines.supervisorScope
 
 internal class RelayClient internal constructor(private val relay: RelayService) : Relay {
 
-    override val eventsFlow: SharedFlow<RelayEvent> = relay
+    override val eventsFlow: SharedFlow<WalletConnect.Model.Relay.Event> = relay
         .observeWebSocketEvent()
         .map { it.toRelayEvent() }
         .shareIn(scope, SharingStarted.Lazily, REPLAY)
 
-    override val subscriptionRequest: Flow<RelayDTO.Subscription.Request> =
+    override val subscriptionRequest: Flow<WalletConnect.Model.Relay.DTO.Subscription.Request> =
         relay.observeSubscriptionRequest()
+            .map { it.toRelayRequest() }
             .onEach { relayRequest -> supervisorScope { publishSubscriptionAcknowledgement(relayRequest.id) } }
 
-    override fun publish(topic: TopicVO, message: String, prompt: Boolean, onResult: (Result<RelayDTO.Publish.Acknowledgement>) -> Unit) {
-        val request = RelayDTO.Publish.Request(generateId(), params = RelayDTO.Publish.Request.Params(topic, message, prompt = prompt))
+    override fun publish(topic: String, message: String, prompt: Boolean, onResult: (Result<WalletConnect.Model.Relay.DTO.Publish.Acknowledgement>) -> Unit) {
+        val request = RelayDTO.Publish.Request(generateId(), params = RelayDTO.Publish.Request.Params(
+            TopicVO(topic), message, prompt = prompt))
         observePublishAcknowledgement { acknowledgement -> onResult(Result.success(acknowledgement)) }
         observePublishError { error -> onResult(Result.failure(error)) }
         relay.publishRequest(request)
     }
 
-    override fun subscribe(topic: TopicVO, onResult: (Result<RelayDTO.Subscribe.Acknowledgement>) -> Unit) {
-        val subscribeRequest = RelayDTO.Subscribe.Request(id = generateId(), params = RelayDTO.Subscribe.Request.Params(topic))
+    override fun subscribe(topic: String, onResult: (Result<WalletConnect.Model.Relay.DTO.Subscribe.Acknowledgement>) -> Unit) {
+        val subscribeRequest = RelayDTO.Subscribe.Request(id = generateId(), params = RelayDTO.Subscribe.Request.Params(TopicVO(topic)))
         observeSubscribeAcknowledgement { acknowledgement -> onResult(Result.success(acknowledgement)) }
         observeSubscribeError { error -> onResult(Result.failure(error)) }
         relay.subscribeRequest(subscribeRequest)
     }
 
     override fun unsubscribe(
-        topic: TopicVO,
-        subscriptionId: SubscriptionIdVO,
-        onResult: (Result<RelayDTO.Unsubscribe.Acknowledgement>) -> Unit,
+        topic: String,
+        subscriptionId: String,
+        onResult: (Result<WalletConnect.Model.Relay.DTO.Unsubscribe.Acknowledgement>) -> Unit,
     ) {
         val unsubscribeRequest =
-            RelayDTO.Unsubscribe.Request(id = generateId(), params = RelayDTO.Unsubscribe.Request.Params(topic, subscriptionId))
+            RelayDTO.Unsubscribe.Request(id = generateId(), params = RelayDTO.Unsubscribe.Request.Params(TopicVO(topic), SubscriptionIdVO(subscriptionId)))
         observeUnSubscribeAcknowledgement { acknowledgement -> onResult(Result.success(acknowledgement)) }
         observeUnSubscribeError { error -> onResult(Result.failure(error)) }
         relay.unsubscribeRequest(unsubscribeRequest)
@@ -57,9 +61,10 @@ internal class RelayClient internal constructor(private val relay: RelayService)
         relay.publishSubscriptionAcknowledgement(publishRequest)
     }
 
-    private fun observePublishAcknowledgement(onResult: (RelayDTO.Publish.Acknowledgement) -> Unit) {
+    private fun observePublishAcknowledgement(onResult: (WalletConnect.Model.Relay.DTO.Publish.Acknowledgement) -> Unit) {
         scope.launch {
             relay.observePublishAcknowledgement()
+                .map { it.toRelayAcknowledgment() }
                 .catch { exception -> Logger.error(exception) }
                 .collect { acknowledgement ->
                     supervisorScope {
@@ -84,9 +89,10 @@ internal class RelayClient internal constructor(private val relay: RelayService)
         }
     }
 
-    private fun observeSubscribeAcknowledgement(onResult: (RelayDTO.Subscribe.Acknowledgement) -> Unit) {
+    private fun observeSubscribeAcknowledgement(onResult: (WalletConnect.Model.Relay.DTO.Subscribe.Acknowledgement) -> Unit) {
         scope.launch {
             relay.observeSubscribeAcknowledgement()
+                .map { it.toRelayAcknowledgment() }
                 .catch { exception -> Logger.error(exception) }
                 .collect { acknowledgement ->
                     supervisorScope {
@@ -111,9 +117,10 @@ internal class RelayClient internal constructor(private val relay: RelayService)
         }
     }
 
-    private fun observeUnSubscribeAcknowledgement(onSuccess: (RelayDTO.Unsubscribe.Acknowledgement) -> Unit) {
+    private fun observeUnSubscribeAcknowledgement(onSuccess: (WalletConnect.Model.Relay.DTO.Unsubscribe.Acknowledgement) -> Unit) {
         scope.launch {
             relay.observeUnsubscribeAcknowledgement()
+                .map { it.toRelayAcknowledgment() }
                 .catch { exception -> Logger.error(exception) }
                 .collect { acknowledgement ->
                     supervisorScope {
