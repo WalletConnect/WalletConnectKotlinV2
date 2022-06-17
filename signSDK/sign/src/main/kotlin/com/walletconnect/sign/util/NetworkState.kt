016367f8ac224a1ddcbc4559f6004aa16ea6d9d8
kubel <jakubwitczak6@gmail.com>
2022-06-17 12:42:52 +0200

    Clean up

--- signSDK/sign/src/main/kotlin/com/walletconnect/sign/util/NetworkState.kt
+++ signSDK/sign/src/main/kotlin/com/walletconnect/sign/util/NetworkState.kt
@@ -10,33 +10,30 @@ import kotlinx.coroutines.flow.*
 
 internal class NetworkState(context: Context) {
 
+    private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     private val _isWiFiAvailable = MutableStateFlow(false)
     private val _isCellularAvailable = MutableStateFlow(false)
 
-    //    private val _isAvailable = MutableStateFlow(false)
     val isAvailable: StateFlow<Boolean> = combine(_isWiFiAvailable, _isCellularAvailable) { wifi, cellular ->
         if (!wifi && !cellular) false
-        else wifi || cellular
+        else if (wifi) true
+        else cellular
     }.stateIn(scope, SharingStarted.Eagerly, false)
 
-    private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
     private val callback = object : ConnectivityManager.NetworkCallback() {
+
         override fun onAvailable(network: Network) {
 
             connectivityManager.getNetworkCapabilities(network)?.let { capabilities ->
 
                 if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    Logger.error("kobe; onWifiAvailable; ${_isWiFiAvailable.value}")
                     _isWiFiAvailable.compareAndSet(expect = false, update = true)
                 }
 
                 if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                    Logger.error("kobe; onCellularAvailable; ${_isCellularAvailable.value}")
                     _isCellularAvailable.compareAndSet(expect = false, update = true)
                 }
             }
-//            _isAvailable.compareAndSet(expect = false, update = true)
         }
 
         override fun onLost(network: Network) {
@@ -44,21 +41,17 @@ internal class NetworkState(context: Context) {
             connectivityManager.getNetworkCapabilities(network)?.let { capabilities ->
 
                 if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    Logger.error("kobe; onWifiLost; ${_isWiFiAvailable.value}")
                     _isWiFiAvailable.compareAndSet(expect = true, update = false)
                 }
 
                 if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                    Logger.error("kobe; onCellularLost; ${_isCellularAvailable.value}")
                     _isCellularAvailable.compareAndSet(expect = true, update = false)
                 }
             }
-//            _isAvailable.compareAndSet(expect = true, update = false)
         }
     }
 
     init {
-
         val request = NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
@@ -66,16 +59,7 @@ internal class NetworkState(context: Context) {
             .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
             .build()
 
-//        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).registerDefaultNetworkCallback(callback)
-
         connectivityManager.registerNetworkCallback(request, callback)
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//
-//        } else {
-//
-//
-//        }
     }
 }
 
