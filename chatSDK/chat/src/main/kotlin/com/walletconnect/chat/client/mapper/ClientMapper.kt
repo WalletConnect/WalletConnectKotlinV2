80d367e9b2737870749638b966b740e67638e491
kubel <jakubwitczak6@gmail.com>
2022-07-04 06:17:11 +0200

    Change todo comment inside client mappers

--- chatSDK/chat/src/main/kotlin/com/walletconnect/chat/client/mapper/ClientMapper.kt
+++ chatSDK/chat/src/main/kotlin/com/walletconnect/chat/client/mapper/ClientMapper.kt
@@ -5,16 +5,18 @@ package com.walletconnect.chat.client.mapper
 import com.walletconnect.chat.client.Chat
 import com.walletconnect.chat.engine.model.EngineDO
 
-//TODO: Provide VO objects for engine classes. Remove using the EngineDO object in the client layer
+//TODO: Figure out what to do with models separation
+@JvmSynthetic
 internal fun Chat.Params.Invite.toInviteEngineDO(): EngineDO.Invite {
     return EngineDO.Invite(invite.account.value, invite.message, invite.signature)
 }
 
+@JvmSynthetic
 internal fun Chat.Params.Message.toMessageEngineDO() : EngineDO.SendMessage {
     return EngineDO.SendMessage(message, media.toMediaEngineDO())
 }
 
+@JvmSynthetic
 private fun Chat.Model.Media.toMediaEngineDO() : EngineDO.Media {
     return EngineDO.Media(type, data)
 }
-
