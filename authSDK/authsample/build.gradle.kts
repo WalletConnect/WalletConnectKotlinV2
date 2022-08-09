a762bac7e327c3ef84c48ae7c629b20f1465b3f0
szymon <sz.rzad@gmail.com>
2022-08-09 09:45:10 +0200

    feat: Add jvmVersion from Dependencies.kt

--- authSDK/authsample/build.gradle.kts
+++ authSDK/authsample/build.gradle.kts
@@ -23,11 +23,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = jvmVersion
+        targetCompatibility = jvmVersion
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = jvmVersion.toString()
     }
 }
 
