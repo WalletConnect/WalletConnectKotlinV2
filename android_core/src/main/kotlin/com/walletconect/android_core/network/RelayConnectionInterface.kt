1eb8da77426648d8b3f5221df76cdf2a8f8a8f78
Jakub Witczak <jakubwitczak6@gmail.com>
2022-08-10 10:17:47 +0200

    Update RelayConnectionInterface.kt

--- android_core/src/main/kotlin/com/walletconect/android_core/network/RelayConnectionInterface.kt
+++ android_core/src/main/kotlin/com/walletconect/android_core/network/RelayConnectionInterface.kt
@@ -4,8 +4,7 @@ package com.walletconect.android_core.network
 
 import com.walletconnect.foundation.network.RelayInterface
 
-interface RelayConnectionInterface : RelayInterface
-{
+interface RelayConnectionInterface : RelayInterface {
     fun connect(onError: (String) -> Unit)
     fun disconnect(onError: (String) -> Unit)
 }
