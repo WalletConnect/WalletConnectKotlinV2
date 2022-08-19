09c06f7f49976a4e57700fb629fafb7758a68eae
kubel <jakubwitczak6@gmail.com>
2022-08-19 12:51:49 +0200

    Add missing dependency to auth storage

--- android_core/src/main/kotlin/com/walletconnect/android_core/common/SDKError.kt
+++ android_core/src/main/kotlin/com/walletconnect/android_core/common/SDKError.kt
@@ -1,6 +1,5 @@
 package com.walletconnect.android_core.common
 
-import com.walletconnect.android_core.common.InternalError
 import com.walletconnect.android_core.common.model.type.EngineEvent
 
 class SDKError(val exception: InternalError) : EngineEvent
