9ff1248a4bb360fbbda00f555063beea401caefe
Talha Ali <TAli@loc.gov>
2022-09-02 08:17:55 -0500

    moved logic to load some modules into init of parent Protocol

--- signSDK/sign/build.gradle.kts
+++ signSDK/sign/build.gradle.kts
@@ -26,12 +26,9 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
