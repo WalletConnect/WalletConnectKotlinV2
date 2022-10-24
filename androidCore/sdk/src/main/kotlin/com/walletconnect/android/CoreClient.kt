9d16e29ebc1cb3b9b4e19bfa59e22108ddf90b4f
szymon <sz.rzad@gmail.com>
2022-10-24 14:37:12 +0200

    Change Relay initalization flow

--- androidCore/sdk/src/main/kotlin/com/walletconnect/android/CoreClient.kt
+++ androidCore/sdk/src/main/kotlin/com/walletconnect/android/CoreClient.kt
@@ -10,13 +10,17 @@ import com.walletconnect.android.relay.RelayConnectionInterface
 import org.koin.dsl.module
 
 object CoreClient {
-    var Pairing: PairingInterface = PairingClient
+    val Pairing: PairingInterface = PairingClient
     var Relay: RelayConnectionInterface = RelayClient
 
     interface CoreDelegate : PairingInterface.Delegate
 
-    fun initialize(metaData: Core.Model.AppMetaData, relayServerUrl: String, connectionType: ConnectionType, application: Application) {
-        RelayClient.initialize(relayServerUrl, connectionType, application)
+    fun initialize(metaData: Core.Model.AppMetaData, relayServerUrl: String, connectionType: ConnectionType, application: Application, relay: RelayConnectionInterface? = null) {
+        if (relay != null) {
+            Relay = relay
+        } else {
+            RelayClient.initialize(relayServerUrl, connectionType, application)
+        }
         PairingClient.initialize(metaData)
         wcKoinApp.modules(module {
             single { Pairing }
