671c76e1f33de7a4747c925a0b6cd79e36569d22
kubel <jakubwitczak6@gmail.com>
2022-12-16 11:09:17 +0100

    Add logs

--- androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/adapter/JsonRpcResultAdapter.kt
+++ androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/adapter/JsonRpcResultAdapter.kt
@@ -2,10 +2,7 @@
 
 package com.walletconnect.android.internal.common.adapter
 
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.JsonReader
-import com.squareup.moshi.JsonWriter
-import com.squareup.moshi.Moshi
+import com.squareup.moshi.*
 import com.squareup.moshi.internal.Util
 import com.walletconnect.android.internal.common.JsonRpcResponse
 import com.walletconnect.android.internal.common.model.params.CoreAuthParams
@@ -19,15 +16,30 @@ import kotlin.Long
 import kotlin.String
 
 // TODO: figure out how to share this with other modules
-internal class JsonRpcResultAdapter(moshi: Moshi) : JsonAdapter<JsonRpcResponse.JsonRpcResult>() {
+internal class JsonRpcResultAdapter(val moshi: Moshi) : JsonAdapter<JsonRpcResponse.JsonRpcResult>() {
+
     private val options: JsonReader.Options = JsonReader.Options.of("id", "jsonrpc", "result")
     private val longAdapter: JsonAdapter<Long> = moshi.adapter(Long::class.java, emptySet(), "id")
     private val stringAdapter: JsonAdapter<String> = moshi.adapter(String::class.java, emptySet(), "jsonrpc")
     private val anyAdapter: JsonAdapter<Any> = moshi.adapter(Any::class.java, emptySet(), "result")
 
-    private val approvalParamsAdapter: JsonAdapter<CoreSignParams.ApprovalParams> = moshi.adapter(CoreSignParams.ApprovalParams::class.java)
-    private val cacaoAdapter: JsonAdapter<CoreAuthParams.ResponseParams> = moshi.adapter(CoreAuthParams.ResponseParams::class.java)
-    private val acceptanceParamsAdapter: JsonAdapter<CoreChatParams.AcceptanceParams> = moshi.adapter(CoreChatParams.AcceptanceParams::class.java)
+//    private val approvalParamsAdapter: JsonAdapter<CoreSignParams.ApprovalParams> = moshi.adapter(CoreSignParams.ApprovalParams::class.java)
+//    private val cacaoAdapter: JsonAdapter<CoreAuthParams.ResponseParams> = moshi.adapter(CoreAuthParams.ResponseParams::class.java)
+//    private val acceptanceParamsAdapter: JsonAdapter<CoreChatParams.AcceptanceParams> = moshi.adapter(CoreChatParams.AcceptanceParams::class.java)
+
+
+    init {
+
+        println("kobe; Adding adapters 1")
+
+        moshi.newBuilder()
+            .addLast(CoreSignParams.ApprovalParams::class.java)
+            .addLast(CoreAuthParams.ResponseParams::class.java)
+            .addLast(CoreChatParams.AcceptanceParams::class.java)
+            .build()
+
+        println("kobe; Adding adapters 2")
+    }
 
     @Volatile
     private var constructorRef: Constructor<JsonRpcResponse.JsonRpcResult>? = null
@@ -42,31 +54,42 @@ internal class JsonRpcResultAdapter(moshi: Moshi) : JsonAdapter<JsonRpcResponse.
         var result: Any? = null
         var mask0 = -1
         reader.beginObject()
+
         while (reader.hasNext()) {
+
             when (reader.selectName(options)) {
+
                 0 -> id = longAdapter.fromJson(reader) ?: throw Util.unexpectedNull("id", "id", reader)
                 1 -> {
-                    jsonrpc = stringAdapter.fromJson(reader) ?: throw Util.unexpectedNull(
-                        "jsonrpc",
-                        "jsonrpc", reader
-                    )
+                    jsonrpc = stringAdapter.fromJson(reader) ?: throw Util.unexpectedNull("jsonrpc", "jsonrpc", reader)
                     // $mask = $mask and (1 shl 1).inv()
                     mask0 = mask0 and 0xfffffffd.toInt()
                 }
                 2 -> {
-                    result = try {
-                        approvalParamsAdapter.fromJson(reader)
-                    } catch (e: Exception) {
-                        try {
-                            cacaoAdapter.fromJson(reader)
-                        } catch (e: Exception) {
-                            try {
-                                acceptanceParamsAdapter.fromJson(reader)
-                            } catch (e: Exception) {
-                                anyAdapter.fromJson(reader)
-                            }
-                        }
-                    }
+
+                    result = moshi.adapter(CoreAuthParams.ResponseParams::class.java).fromJson(reader)
+
+//                    try {
+//
+//                        approvalParamsAdapter.fromJson(reader)
+//
+//                    } catch (e: JsonDataException) {
+//
+//                        cacaoAdapter.fromJson(reader)
+//
+////                        try {
+////                            cacaoAdapter.fromJson(reader)
+////                        } catch (e: JsonDataException) {
+////
+////                            try {
+////
+////                                acceptanceParamsAdapter.fromJson(reader)
+////
+////                            } catch (e: JsonDataException) {
+////                                anyAdapter.fromJson(reader)
+////                            }
+////                        }
+//                    }
                 }
                 -1 -> {
                     // Unknown name, skip it.
@@ -76,6 +99,7 @@ internal class JsonRpcResultAdapter(moshi: Moshi) : JsonAdapter<JsonRpcResponse.
             }
         }
         reader.endObject()
+
         if (mask0 == 0xfffffffd.toInt()) {
             // All parameters with defaults are set, invoke the constructor directly
             return JsonRpcResponse.JsonRpcResult(
@@ -117,21 +141,21 @@ internal class JsonRpcResultAdapter(moshi: Moshi) : JsonAdapter<JsonRpcResponse.
 
         when {
             (value_.result as? CoreSignParams.ApprovalParams) != null -> {
-                val approvalParamsString = approvalParamsAdapter.toJson(value_.result)
+                val approvalParamsString = moshi.adapter(CoreSignParams.ApprovalParams::class.java).toJson(value_.result)
                 writer.valueSink().use {
                     it.writeUtf8(approvalParamsString)
                 }
             }
 
             (value_.result as? CoreAuthParams.ResponseParams) != null -> {
-                val responseParamsString = cacaoAdapter.toJson(value_.result)
+                val responseParamsString = moshi.adapter(CoreAuthParams.ResponseParams::class.java).toJson(value_.result)
                 writer.valueSink().use {
                     it.writeUtf8(responseParamsString)
                 }
             }
 
             (value_.result as? CoreChatParams.AcceptanceParams) != null -> {
-                val approvalParamsString = acceptanceParamsAdapter.toJson(value_.result)
+                val approvalParamsString = moshi.adapter(CoreChatParams.AcceptanceParams::class.java).toJson(value_.result)
                 writer.valueSink().use {
                     it.writeUtf8(approvalParamsString)
                 }
