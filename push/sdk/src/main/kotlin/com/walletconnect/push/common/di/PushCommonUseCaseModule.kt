464b81bebc6deee9bb69f0796d596c4370494390
Talha Ali <TAli@loc.gov>
2022-12-20 03:10:28 -0600

    dapp and wallet are able to communicate

---
+++ push/sdk/src/main/kotlin/com/walletconnect/push/common/di/PushCommonUseCaseModule.kt
@@ -0,0 +1,12 @@
+@file:JvmSynthetic
+
+package com.walletconnect.push.common.di
+
+import com.walletconnect.push.common.domain.GetListOfSubscriptionsUseCase
+import org.koin.dsl.module
+
+@JvmSynthetic
+internal fun commonUseCasesModule() = module {
+
+    single { GetListOfSubscriptionsUseCase() }
+}
