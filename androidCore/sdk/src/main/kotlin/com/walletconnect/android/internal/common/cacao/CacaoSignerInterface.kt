d1936b9d1c931943d135077cdb0bc1c3d23bfdf6
Talha Ali <TAli@loc.gov>
2023-01-13 18:29:20 -0600

    Fixed sign function to dynamically create SDK defined signature class

--- androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/cacao/CacaoSignerInterface.kt
+++ androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/cacao/CacaoSignerInterface.kt
@@ -1,5 +1,6 @@
 @file:JvmSynthetic
 @file:Suppress("PackageDirectoryMismatch")
+
 package com.walletconnect.android.cacao
 
 import com.walletconnect.android.cacao.signature.ISignatureType
@@ -8,14 +9,21 @@ import com.walletconnect.android.internal.common.cacao.Cacao
 import com.walletconnect.android.internal.common.cacao.eip191.EIP191Signer
 import com.walletconnect.android.internal.common.cacao.signature.SignatureInterface
 import com.walletconnect.android.internal.common.cacao.signature.toCacaoSignature
-import kotlin.reflect.full.cast
 
 interface CacaoSignerInterface<CoreSignature : SignatureInterface>
 
-@Suppress("UNCHECKED_CAST") // Added to dismiss confusion. Cast to `T` always succeeds as Cacao.Signature implements ISignature.
-inline fun <CoreSignature : SignatureInterface, reified SDKSignature : CoreSignature> CacaoSignerInterface<CoreSignature>.sign(message: String, privateKey: ByteArray, type: ISignatureType):
-        CoreSignature = when (type.header) {
-    SignatureType.EIP191.header, SignatureType.EIP1271.header -> SDKSignature::class.cast(Cacao.Signature(type.header, EIP191Signer.sign(message.toByteArray(), privateKey).toCacaoSignature()))
-    else -> throw Throwable("SignatureType not recognized")
-}
+@Suppress("UNCHECKED_CAST", "unused") // Added to dismiss confusion. Cast to `T` always succeeds as Cacao.Signature implements ISignature.
+inline fun <CoreSignature : SignatureInterface, reified SDKSignature : CoreSignature> CacaoSignerInterface<CoreSignature>.sign(
+    message: String,
+    privateKey: ByteArray,
+    type: ISignatureType,
+): CoreSignature =
+    when (type.header) {
+        SignatureType.EIP191.header, SignatureType.EIP1271.header -> {
+            (Cacao.Signature(type.header, EIP191Signer.sign(message.toByteArray(), privateKey).toCacaoSignature()) as SignatureInterface).run {
+                SDKSignature::class.constructors.first().call(t, s, m)
+            }
+        }
+        else -> throw Throwable("SignatureType not recognized")
+    }
 
