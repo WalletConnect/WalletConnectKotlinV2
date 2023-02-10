1eccbd1d4e8bf8d4ac264fe219658670e7a261b3
kubel <jakubwitczak6@gmail.com>
2023-02-10 16:33:09 +0100

    Clean up

--- sign/sdk/src/main/kotlin/com/walletconnect/sign/client/mapper/ClientMapper.kt
+++ sign/sdk/src/main/kotlin/com/walletconnect/sign/client/mapper/ClientMapper.kt
@@ -2,9 +2,7 @@
 
 package com.walletconnect.sign.client.mapper
 
-import com.walletconnect.android.Core
 import com.walletconnect.android.internal.common.JsonRpcResponse
-import com.walletconnect.android.internal.common.model.AppMetaData
 import com.walletconnect.android.internal.common.model.ConnectionState
 import com.walletconnect.android.internal.common.model.SDKError
 import com.walletconnect.android.pairing.model.mapper.toClient
