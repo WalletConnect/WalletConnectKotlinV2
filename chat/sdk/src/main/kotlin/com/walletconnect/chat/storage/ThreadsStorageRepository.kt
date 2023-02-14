f3866a2ad70d2e3dc82283d474e81beccfbbca8f
szymon <sz.rzad@gmail.com>
2023-02-14 12:39:34 +0100

    feat: Add persistent listening on invites, accepts and chat events

--- chat/sdk/src/main/kotlin/com/walletconnect/chat/storage/ThreadsStorageRepository.kt
+++ chat/sdk/src/main/kotlin/com/walletconnect/chat/storage/ThreadsStorageRepository.kt
@@ -1,31 +1,15 @@
 package com.walletconnect.chat.storage
 
+import com.walletconnect.chat.common.model.AccountId
 import com.walletconnect.chat.common.model.Thread
 import com.walletconnect.chat.storage.data.dao.ThreadsQueries
+import com.walletconnect.foundation.common.model.Topic
 
 internal class ThreadsStorageRepository(private val threads: ThreadsQueries) {
-
-    fun insertThread(topic: String, selfAccount: String, peerAccount: String) {
-        threads.insertOrAbortThread(topic, selfAccount, peerAccount)
-    }
-
-    fun getThreadsForSelfAccount(account: String): List<Thread> {
-        return threads.getThreadsForSelfAccount(account, ::dbToThread).executeAsList()
-    }
-
-    fun deleteThreadByTopic(topic: String) {
-        threads.deleteThreadByTopic(topic)
-    }
-
-    fun getThreadByTopic(topic: String): Thread {
-        return threads.getThreadByTopic(topic, ::dbToThread).executeAsOne()
-    }
-
-    private fun dbToThread(
-        topic: String,
-        selfPublicKey: String,
-        peerPublicKey: String
-    ): Thread {
-        return Thread(topic, selfPublicKey, peerPublicKey)
-    }
+    fun insertThread(topic: String, selfAccount: String, peerAccount: String) = threads.insertOrAbortThread(topic, selfAccount, peerAccount)
+    fun getThreadsForSelfAccount(account: String): List<Thread> = threads.getThreadsForSelfAccount(account, ::dbToThread).executeAsList()
+    fun deleteThreadByTopic(topic: String) = threads.deleteThreadByTopic(topic)
+    fun getThreadByTopic(topic: String): Thread = threads.getThreadByTopic(topic, ::dbToThread).executeAsOne()
+    fun getAllThreads(): List<Thread> = threads.getAllThreads(::dbToThread).executeAsList()
+    private fun dbToThread(topic: String, selfPublicKey: String, peerPublicKey: String) = Thread(Topic(topic), AccountId(selfPublicKey), AccountId(peerPublicKey))
 }
