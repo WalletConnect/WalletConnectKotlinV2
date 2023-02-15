07463251622e8db141e4ec2c8ad8e9f812ad472b
kubel <jakubwitczak6@gmail.com>
2023-02-15 13:22:28 +0100

    Update rpc id

---
+++ foundation/src/test/kotlin/com/walletconnect/foundation/UtilsTest.kt
@@ -0,0 +1,15 @@
+package com.walletconnect.foundation
+
+import com.walletconnect.util.generateId
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class UtilsTest {
+
+    @Test
+    fun `generate id test`() {
+        val id = generateId()
+        println(id)
+        assertTrue { id > 0 }
+    }
+}
