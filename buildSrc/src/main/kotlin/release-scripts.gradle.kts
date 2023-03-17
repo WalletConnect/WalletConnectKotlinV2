cbaaad8e234a020145c9c162d0cfd6f891dc2039
Talha Ali <>
2023-03-17 18:21:45 -0500

    Updated release script to by dynamic based on parameter passed to task

--- buildSrc/src/main/kotlin/release-scripts.gradle.kts
+++ buildSrc/src/main/kotlin/release-scripts.gradle.kts
@@ -1,49 +1,54 @@
-tasks.register("releaseLocal") {
+import org.apache.tools.ant.taskdefs.condition.Os
+import kotlin.reflect.full.safeCast
+
+// Example ./gradlew releaseAllSDKs -Ptype=local
+// Example ./gradlew releaseAllSDKs -Ptype=sonatype
+tasks.register("releaseAllSDKs") {
     doLast {
-        generateListOfModuleTasks("local")
-            .map { it.path }
-            .forEach { task ->
-                println("Releasing $task")
-                exec {
-                    commandLine("./gradlew", task)
-                }
+        project.findProperty("type")
+            ?.run(String::class::safeCast)
+            ?.run {
+                println("Converting parameter to an supported ReleaseType value")
+                ReleaseType.valueOf(this.toUpperCase())
             }
+            ?.let { releaseType ->
+                generateListOfModuleTasks(releaseType).forEach { task ->
+                    println("Executing Task: $task")
+                    exec {
+                        val gradleCommand = if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+                            "gradlew.bat"
+                        } else {
+                            "./gradlew"
+                        }
+                        commandLine(gradleCommand, task.path)
+                    }
+                }
+            } ?: throw Exception("Missing Type parameter")
     }
 }
 
-tasks.register("releaseRemote") {
-    doLast {
-        generateListOfModuleTasks("release")
-            .map { it.path }
-            .forEach { task ->
-                println("Releasing $task")
-                exec {
-                    commandLine("./gradlew", task)
-                }
-            }
-    }
-}
+fun generateListOfModuleTasks(type: ReleaseType): List<Task> = compileListOfSDKs().extractListOfPublishingTasks(type)
 
-fun generateListOfModuleTasks(type: String): List<Task> = compileListOfSDKs().extractListOfPublishingTasks(type)
-
+// Triple consists of the root module name, the child module name, and if it's a JVM or Android module
 fun compileListOfSDKs(): List<Triple<String, String?, String>> = listOf(
     Triple("foundation", null, "jvm"),
     Triple("androidCore", "sdk", "android"),
     Triple("sign", "sdk", "android"),
     Triple("auth", "sdk", "android"),
-//    Triple("chat", "sdk", "android"),
+    Triple("chat", "sdk", "android"),
     Triple("web3", "wallet", "android"),
-//    Triple("web3", "inbox", "android"),
+    Triple("web3", "inbox", "android"),
     Triple("androidCore", "bom", "jvm"),
 )
 
-fun List<Triple<String, String?, String>>.extractListOfPublishingTasks(type: String): List<Task> = map { (parentModule, childModule, env) ->
+// This extension function will determine which task to run based on the type passed
+fun List<Triple<String, String?, String>>.extractListOfPublishingTasks(type: ReleaseType): List<Task> = map { (parentModule, childModule, env) ->
     val task = when {
-        env == "jvm" && type == "local" -> "publishMavenJvmPublicationToMavenLocal"
-        env == "jvm" && type == "release" -> "publishMavenJvmPublicationToSonatypeRepository"
-        env == "android" && type == "local" -> "publishReleasePublicationToMavenLocal"
-        env == "android" && type == "release" -> "publishReleasePublicationToSonatypeRepository"
-        else -> throw Exception("Unknown Type")
+        env == "jvm" && type == ReleaseType.LOCAL -> "${publishJvmRoot}MavenLocal"
+        env == "jvm" && type == ReleaseType.SONATYPE -> "${publishJvmRoot}SonatypeRepository"
+        env == "android" && type == ReleaseType.LOCAL -> "${publishAndroidRoot}MavenLocal"
+        env == "android" && type == ReleaseType.SONATYPE -> "${publishAndroidRoot}SonatypeRepository"
+        else -> throw Exception("Unknown Type or Env")
     }
 
     val module = if (childModule != null) {
@@ -53,4 +58,11 @@ fun List<Triple<String, String?, String>>.extractListOfPublishingTasks(type: Str
     }
 
     module.tasks.getByName(task)
+}
+
+private val publishJvmRoot = "publishMavenJvmPublicationTo"
+private val publishAndroidRoot = "publishReleasePublicationTo"
+
+enum class ReleaseType {
+    LOCAL, SONATYPE
 }
