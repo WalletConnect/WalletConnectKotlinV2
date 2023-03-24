f5f60c665ca5fa38574abb9fccf444dea209fd38
Talha Ali <>
2023-03-24 17:45:32 -0500

    Created new sample wallet app

---
+++ samples/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/Route.kt
@@ -0,0 +1,32 @@
+package com.walletconnect.sample.wallet.ui.routes
+
+import androidx.navigation.NavController
+
+sealed class Route(val path: String) {
+    object GetStarted : Route("get_started")
+    object Connections : Route("connections")
+    object SessionProposal : Route("session_proposal")
+    object SessionRequest : Route("session_request")
+    object AuthRequest : Route("auth_request")
+    object PasteUri : Route("paste_uri")
+    object ScanUri : Route("scan_uri")
+    object PushRequest : Route("push_request") {
+        const val KEY_REQUEST_ID: String = "request_id"
+        const val KEY_PEER_NAME: String = "peer_name"
+        const val KEY_PEER_DESC: String = "peer_desc"
+        const val KEY_ICON_URL: String = "icon_url"
+        const val KEY_REDIRECT: String = "redirect"
+    }
+    object ConnectionDetails : Route("connection_details")
+    object SnackbarMessage : Route("snackbar_message")
+    object Web3Inbox : Route("web3inbox")
+}
+
+fun NavController.showSnackbar(message: String) {
+    navigate("${Route.SnackbarMessage.path}/$message")
+}
+
+
+
+
+
---
+++ samples/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/dialog_routes/snackbar_message/SnackbarMessageRoute.kt
@@ -0,0 +1,33 @@
+package com.walletconnect.sample.wallet.ui.routes.dialog_routes.snackbar_message
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Snackbar
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.walletconnect.sample.wallet.ui.common.themedColor
+import com.walletconnect.util.Empty
+
+@Composable
+fun SnackbarMessageRoute(navController: NavController, message: String?) {
+    Snackbar(
+        modifier = Modifier.padding(start = 10.dp, top = 0.dp, end = 10.dp, bottom = 40.dp),
+        action = {
+            Button(onClick = {
+                navController.popBackStack()
+            }, colors = ButtonDefaults.buttonColors(
+                backgroundColor = themedColor(darkColor = Color(0xFF9E9E9E), lightColor = Color(0xFF000000)),
+            )) {
+                Text("OK")
+            }
+        },
+    ) {
+        Text(text = message ?: String.Empty)
+    }
+
+}
