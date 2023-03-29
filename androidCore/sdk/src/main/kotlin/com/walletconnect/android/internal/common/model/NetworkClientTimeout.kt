493a8fee4e57dc4d6e2d7763d30b60d535270a37
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2023-03-29 13:01:27 +0300

    ♻️ Add validation to NetworkClientTimeout timeout

--- androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/model/NetworkClientTimeout.kt
+++ androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/model/NetworkClientTimeout.kt
@@ -7,9 +7,24 @@ data class NetworkClientTimeout(
     val timeUnit: TimeUnit
 ) {
 
+    init {
+        require(isTimeoutInRequiredRange()) {
+            "Timeout must be in range of $MIN_TIMEOUT_LIMIT_AS_MILLIS .. $MAX_TIMEOUT_LIMIT_AS_MILLIS milliseconds"
+        }
+    }
+
+    private fun isTimeoutInRequiredRange(): Boolean {
+        val timeoutAsMillis = TimeUnit.MILLISECONDS.convert(timeout, timeUnit)
+        return timeoutAsMillis in MIN_TIMEOUT_LIMIT_AS_MILLIS..MAX_TIMEOUT_LIMIT_AS_MILLIS
+    }
+
     companion object {
+
+        private const val MIN_TIMEOUT_LIMIT_AS_MILLIS = 5_000L
+        private const val MAX_TIMEOUT_LIMIT_AS_MILLIS = 60_000L
+
         fun getDefaultTimeout() = NetworkClientTimeout(
-            timeout = 5000L,
+            timeout = MIN_TIMEOUT_LIMIT_AS_MILLIS,
             timeUnit = TimeUnit.MILLISECONDS
         )
     }
