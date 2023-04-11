163bf7313739cb5eb007cf583e87cf36972cf1cb
szymon <sz.rzad@gmail.com>
2023-04-11 12:54:12 +0100

    fix: Ethereum account generation and storage

--- samples/wallet/src/main/kotlin/com/walletconnect/sample/wallet/domain/TestAccounts.kt
+++ samples/wallet/src/main/kotlin/com/walletconnect/sample/wallet/domain/TestAccounts.kt
@@ -4,22 +4,24 @@ import com.walletconnect.sample_common.Chains
 import com.walletconnect.util.hexToBytes
 import io.ipfs.multibase.Base16
 
-val ACCOUNTS_1_EIP155_ADDRESS = EthAccountDelegate.account
+val ACCOUNTS_1_EIP155_ADDRESS by lazy { EthAccountDelegate.account }
 
-val accounts: List<Pair<Chains, String>> = listOf(
-    Chains.ETHEREUM_MAIN to ACCOUNTS_1_EIP155_ADDRESS,
-    Chains.POLYGON_MATIC to ACCOUNTS_1_EIP155_ADDRESS,
-    Chains.ETHEREUM_KOVAN to ACCOUNTS_1_EIP155_ADDRESS,
-    Chains.POLYGON_MUMBAI to ACCOUNTS_1_EIP155_ADDRESS,
-    Chains.COSMOS to "cosmos1w605a5ejjlhp04eahjqxhjhmg8mj6nqhp8v6xc",
-    Chains.BNB to ACCOUNTS_1_EIP155_ADDRESS
-)
+val accounts: List<Pair<Chains, String>> by lazy {
+    listOf(
+        Chains.ETHEREUM_MAIN to ACCOUNTS_1_EIP155_ADDRESS,
+        Chains.POLYGON_MATIC to ACCOUNTS_1_EIP155_ADDRESS,
+        Chains.ETHEREUM_KOVAN to ACCOUNTS_1_EIP155_ADDRESS,
+        Chains.POLYGON_MUMBAI to ACCOUNTS_1_EIP155_ADDRESS,
+        Chains.COSMOS to "cosmos1w605a5ejjlhp04eahjqxhjhmg8mj6nqhp8v6xc",
+        Chains.BNB to ACCOUNTS_1_EIP155_ADDRESS
+    )
+}
 
-val PRIVATE_KEY_1: ByteArray = EthAccountDelegate.privateKey.hexToBytes()
+val PRIVATE_KEY_1: ByteArray by lazy { EthAccountDelegate.privateKey.hexToBytes() }
 
 const val ISS_DID_PREFIX = "did:pkh:"
 
-val ISSUER = accounts.map { it.toIssuer() }.first()
+val ISSUER by lazy { accounts.map { it.toIssuer() }.first() }
 
 fun Pair<Chains, String>.toIssuer(): String = "$ISS_DID_PREFIX${first.chainId}:$second"
 
