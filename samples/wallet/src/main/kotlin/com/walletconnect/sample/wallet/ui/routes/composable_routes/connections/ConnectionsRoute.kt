1523e122b13f1d91fda84c68f9a98e0d5808242c
szymon <sz.rzad@gmail.com>
2023-04-12 14:41:01 +0100

    feat: Add current bom version

--- samples/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/composable_routes/connections/ConnectionsRoute.kt
+++ samples/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/composable_routes/connections/ConnectionsRoute.kt
@@ -32,12 +32,12 @@ import androidx.constraintlayout.compose.ConstraintLayoutScope
 import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavController
 import com.skydoves.landscapist.glide.GlideImage
+import com.walletconnect.sample.wallet.R
 import com.walletconnect.sample.wallet.ui.Web3WalletViewModel
+import com.walletconnect.sample.wallet.ui.common.WCTopAppBar
 import com.walletconnect.sample.wallet.ui.common.findActivity
 import com.walletconnect.sample.wallet.ui.common.themedColor
 import com.walletconnect.sample.wallet.ui.routes.Route
-import com.walletconnect.sample.wallet.R
-import com.walletconnect.sample.wallet.ui.common.WCTopAppBar
 
 @Composable
 fun ConnectionsRoute(navController: NavController, connectionsViewModel: ConnectionsViewModel, web3WalletViewModel: Web3WalletViewModel) {
@@ -64,7 +64,7 @@ fun ConstraintLayoutScope.Title(titleRef: ConstrainedLayoutReference) {
         top.linkTo(parent.top, margin = 0.dp)
         start.linkTo(parent.start)
     }) {
-        WCTopAppBar(text = "Connections")
+        WCTopAppBar(titleText = "Connections")
     }
 }
 
