c2546c21ece4586a824da499b95f290647a7d00d
szymon <sz.rzad@gmail.com>
2023-04-12 16:00:26 +0100

    feat: Send wc_syncDelete request after local storage update

--- sync/sdk/src/main/kotlin/com/walletconnect/sync/engine/use_case/calls/DeleteStoreValueUseCase.kt
+++ sync/sdk/src/main/kotlin/com/walletconnect/sync/engine/use_case/calls/DeleteStoreValueUseCase.kt
@@ -4,11 +4,12 @@ import com.walletconnect.android.internal.common.model.AccountId
 import com.walletconnect.android.internal.common.scope
 import com.walletconnect.sync.common.exception.validateAccountId
 import com.walletconnect.sync.common.model.Store
+import com.walletconnect.sync.engine.use_case.requests.outgoing.SendDeleteRequestUseCase
 import com.walletconnect.sync.storage.StoresStorageRepository
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 
-internal class DeleteStoreValueUseCase(private val storesRepository: StoresStorageRepository) : DeleteUseCaseInterface {
+internal class DeleteStoreValueUseCase(private val storesRepository: StoresStorageRepository, private val sendDeleteRequestUseCase: SendDeleteRequestUseCase) : DeleteUseCaseInterface {
 
     // https://github.com/WalletConnect/WalletConnectKotlinV2/issues/800 -> update params to have StoreKey and StoreValue
     override fun delete(accountId: AccountId, store: Store, key: String, onSuccess: (Boolean) -> Unit, onFailure: (Throwable) -> Unit) {
@@ -21,7 +22,7 @@ internal class DeleteStoreValueUseCase(private val storesRepository: StoresStora
 
                 // Return true in onSuccess when the value was deleted
                 runCatching { storesRepository.deleteStoreValue(accountId, store, key) }.fold(
-                    onSuccess = { onSuccess(true) },
+                    onSuccess = { sendDeleteRequestUseCase(key, accountId, store, onSuccess = { onSuccess(true) }, onError = onFailure) },
                     onFailure = { error -> onFailure(error) }
                 )
             }
