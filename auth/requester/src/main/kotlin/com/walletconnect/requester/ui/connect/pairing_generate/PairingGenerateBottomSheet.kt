57db11580076da45f39c9f00b5130132481d64e1
kubel <jakubwitczak6@gmail.com>
2023-05-11 12:33:20 +0200

    Fix expiry validation

--- auth/requester/src/main/kotlin/com/walletconnect/requester/ui/connect/pairing_generate/PairingGenerateBottomSheet.kt
+++ auth/requester/src/main/kotlin/com/walletconnect/requester/ui/connect/pairing_generate/PairingGenerateBottomSheet.kt
@@ -1,6 +1,10 @@
 package com.walletconnect.requester.ui.connect.pairing_generate
 
-import android.content.*
+import android.content.ActivityNotFoundException
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -33,7 +37,7 @@ class PairingGenerateBottomSheet : BottomSheetDialogFragment() {
             val data = QrData.Url(uri)
 
             viewLifecycleOwner.lifecycleScope.launch {
-                val threadPolicy = when(Runtime.getRuntime().availableProcessors()){
+                val threadPolicy = when (Runtime.getRuntime().availableProcessors()) {
                     in 1..3 -> ThreadPolicy.SingleThread
                     in 4..6 -> ThreadPolicy.DoubleThread
                     else -> ThreadPolicy.QuadThread
@@ -55,6 +59,9 @@ class PairingGenerateBottomSheet : BottomSheetDialogFragment() {
 
             btnDeepLink.setOnClickListener {
                 try {
+
+                    println("kobe: Requester deep link: ${deeplinkPairingUri.toUri()}")
+
                     requireActivity().startActivity(Intent(Intent.ACTION_VIEW, deeplinkPairingUri.toUri()))
                 } catch (exception: ActivityNotFoundException) {
                     // There is no app to handle deep link
