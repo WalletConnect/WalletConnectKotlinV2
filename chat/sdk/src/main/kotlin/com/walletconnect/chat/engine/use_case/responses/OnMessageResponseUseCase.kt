376d08324c6655128891b6e8c5e112921beb72d1
szymon <sz.rzad@gmail.com>
2023-05-16 17:08:47 +0200

    feat: Rename sync stores

--- chat/sdk/src/main/kotlin/com/walletconnect/chat/engine/use_case/responses/OnMessageResponseUseCase.kt
+++ chat/sdk/src/main/kotlin/com/walletconnect/chat/engine/use_case/responses/OnMessageResponseUseCase.kt
@@ -2,12 +2,16 @@ package com.walletconnect.chat.engine.use_case.responses
 
 import com.walletconnect.android.internal.common.JsonRpcResponse
 import com.walletconnect.android.internal.common.model.WCResponse
+import com.walletconnect.android.internal.utils.THIRTY_SECONDS
 import com.walletconnect.foundation.util.Logger
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
 
 internal class OnMessageResponseUseCase(
     private val logger: Logger,
 ) {
     operator fun invoke(wcResponse: WCResponse) {
+        TimeUnit.SECONDS.toMillis(THIRTY_SECONDS)
         when (val response = wcResponse.response) {
             is JsonRpcResponse.JsonRpcError -> logger.log("Message error response: $response")
             is JsonRpcResponse.JsonRpcResult -> logger.log("Message success response")
