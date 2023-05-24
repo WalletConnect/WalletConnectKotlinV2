dda224cbad56bf9459979494d15f0b132dc4eef6
Talha Ali <>
2023-05-24 00:23:25 -0500

    removed commented code from UtilFunctions.kt

--- foundation/src/main/kotlin/com/walletconnect/foundation/util/UtilFunctions.kt
+++ foundation/src/main/kotlin/com/walletconnect/foundation/util/UtilFunctions.kt
@@ -18,7 +18,6 @@ fun generateClientToServerId(): Long {
     return now.plus((100000..999999).random()).toLong()
 }
 
-
 fun randomBytes(size: Int): ByteArray = ByteArray(size).apply {
     SecureRandom().nextBytes(this)
 }
@@ -27,22 +26,6 @@ fun ByteArray.bytesToHex(): String = Base16.encode(this)
 
 fun String.hexToBytes(): ByteArray = Base16.decode(this.lowercase())
 
-//fun ByteArray.bytesToHex(): String {
-//    val hexString = StringBuilder(2 * this.size)
-//
-//    this.indices.forEach { i ->
-//        val hex = Integer.toHexString(0xff and this[i].toInt())
-//
-//        if (hex.length == 1) {
-//            hexString.append('0')
-//        }
-//
-//        hexString.append(hex)
-//    }
-//
-//    return hexString.toString()
-//}
-
 fun ByteArray.bytesToInt(size: Int): Int {
     require(this.size <= 4) { "Byte array size must be less than 5" }
 
@@ -50,19 +33,6 @@ fun ByteArray.bytesToInt(size: Int): Int {
         acc.or(this[i].toInt().shl((size - 1 - i) * 8))
     }
 }
-
-//fun String.hexToBytes(): ByteArray {
-//    val len = this.length
-//    val data = ByteArray(len / 2)
-//    var i = 0
-//
-//    while (i < len) {
-//        data[i / 2] = ((Character.digit(this[i], 16) shl 4) + Character.digit(this[i + 1], 16)).toByte()
-//        i += 2
-//    }
-//
-//    return data
-//}
 
 @JvmSynthetic
 internal fun String.addUserAgent(sdkVersion: String): String {
