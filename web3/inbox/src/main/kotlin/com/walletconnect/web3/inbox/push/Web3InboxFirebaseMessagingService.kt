d747804c0e7f867bfb4bb93c6f43c94ea2f16ffe
szymon <sz.rzad@gmail.com>
2023-06-01 11:09:47 +0200

    feat: Change to PushMessageService

--- web3/inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/Web3InboxFirebaseMessagingService.kt
+++ web3/inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/Web3InboxFirebaseMessagingService.kt
@@ -1,13 +1,14 @@
 package com.walletconnect.web3.inbox.push
 
 import android.annotation.SuppressLint
-import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.walletconnect.push.common.Push
+import com.walletconnect.push.wallet.client.PushMessageService
 
 
 //todo this will need to be implemented by sdk consumers, add to docs
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
-abstract class Web3InboxFirebaseMessagingService : FirebaseMessagingService() {
+abstract class Web3InboxFirebaseMessagingService : PushMessageService() {
     sealed interface Web3InboxMessage {
         data class ChatMessage(val message: String) : Web3InboxMessage
 //        data class PushMessage(val title: String, val body: String, val icon: String?, val url: String?) : Web3InboxMessage
@@ -15,9 +16,12 @@ abstract class Web3InboxFirebaseMessagingService : FirebaseMessagingService() {
 
     abstract fun onMessage(message: Web3InboxMessage, originalMessage: RemoteMessage)
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-
+    override fun onDefaultBehavior(message: RemoteMessage) {
         onMessage(Web3InboxMessage.ChatMessage("You got a new message!!!"), message)
+    }
+
+
+    override fun onMessage(message: Push.Model.Message, originalMessage: RemoteMessage) {
+        // hide
     }
 }
