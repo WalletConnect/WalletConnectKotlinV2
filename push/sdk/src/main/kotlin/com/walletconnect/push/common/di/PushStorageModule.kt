c7f403d06478f5f74ca063e9a6a088d675d442f1
Talha Ali <>
2023-06-20 22:10:43 -0500

    Updated pushStorageModule to include ProposalStorageRepository

--- push/sdk/src/main/kotlin/com/walletconnect/push/common/di/PushStorageModule.kt
+++ push/sdk/src/main/kotlin/com/walletconnect/push/common/di/PushStorageModule.kt
@@ -6,13 +6,16 @@ import com.squareup.sqldelight.ColumnAdapter
 import com.walletconnect.android.di.AndroidCoreDITags
 import com.walletconnect.android.di.sdkBaseStorageModule
 import com.walletconnect.android.internal.common.di.deleteDatabase
+import com.walletconnect.android.internal.common.model.AppMetaData
 import com.walletconnect.push.PushDatabase
+import com.walletconnect.push.common.data.storage.ProposalStorageRepository
 import com.walletconnect.push.common.data.storage.SubscriptionStorageRepository
 import com.walletconnect.push.common.storage.data.dao.Subscriptions
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
+@Suppress("RemoveExplicitTypeArguments")
 @JvmSynthetic
 internal fun pushStorageModule(dbName: String) = module {
     fun Scope.createPushDB() = PushDatabase(
@@ -20,6 +23,9 @@ internal fun pushStorageModule(dbName: String) = module {
         SubscriptionsAdapter = Subscriptions.Adapter(
             metadata_iconsAdapter = get(named(AndroidCoreDITags.COLUMN_ADAPTER_LIST)),
             map_of_scopeAdapter = get<ColumnAdapter<Map<String, Pair<String, Boolean>>, String>>()
+        ),
+        ProposalAdapter = com.walletconnect.push.common.storage.data.dao.Proposal.Adapter(
+            dapp_metadataAdapter = get<ColumnAdapter<AppMetaData, String>>(named(AndroidCoreDITags.COLUMN_ADAPTER_APPMETADATA))
         )
     )
 
@@ -30,7 +36,7 @@ internal fun pushStorageModule(dbName: String) = module {
             override fun decode(databaseValue: String): Map<String, Pair<String, Boolean>> {
                 // Split string by | to get each entry
                 return databaseValue.split("|").associate { entry ->
-                   // Split each entry by = to get key and value
+                    // Split each entry by = to get key and value
                     val entries = entry.split("=")
                     entries.first().trim() to entries.last().split(",").run {
                         // Split value by , to get description and isSubscribed
@@ -60,5 +66,9 @@ internal fun pushStorageModule(dbName: String) = module {
 
     single { get<PushDatabase>().subscriptionsQueries }
 
+    single { get<PushDatabase>().proposalQueries }
+
     single { SubscriptionStorageRepository(get()) }
+
+    single { ProposalStorageRepository(get()) }
 }
