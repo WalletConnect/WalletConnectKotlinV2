78f648bb3a86ee9e733a08b59f7b91293a16556b
szymon <sz.rzad@gmail.com>
2023-07-10 16:47:27 +0200

    feat: Fetch messages on new subscription

--- push/sdk/src/main/kotlin/com/walletconnect/push/wallet/engine/sync/use_case/events/OnSubscriptionUpdateEventUseCase.kt
+++ push/sdk/src/main/kotlin/com/walletconnect/push/wallet/engine/sync/use_case/events/OnSubscriptionUpdateEventUseCase.kt
@@ -1,21 +1,30 @@
 package com.walletconnect.push.wallet.engine.sync.use_case.events
 
 import com.squareup.moshi.Moshi
+import com.walletconnect.android.history.HistoryInterface
+import com.walletconnect.android.history.network.model.messages.MessagesParams
 import com.walletconnect.android.internal.common.crypto.kmr.KeyManagementRepository
+import com.walletconnect.android.internal.common.model.AppMetaDataType
 import com.walletconnect.android.internal.common.model.SymmetricKey
 import com.walletconnect.android.internal.common.model.type.JsonRpcInteractorInterface
+import com.walletconnect.android.internal.common.storage.MetadataStorageRepositoryInterface
 import com.walletconnect.android.sync.common.model.Events
 import com.walletconnect.android.sync.common.model.SyncUpdate
 import com.walletconnect.foundation.common.model.Topic
 import com.walletconnect.foundation.util.Logger
 import com.walletconnect.push.common.data.storage.SubscribeStorageRepository
 import com.walletconnect.push.common.model.toDb
+import com.walletconnect.push.wallet.data.MessagesRepository
 import com.walletconnect.push.wallet.engine.sync.model.SyncedSubscription
 import com.walletconnect.push.wallet.engine.sync.model.toCommon
+import timber.log.Timber
 
 internal class OnSubscriptionUpdateEventUseCase(
     private val logger: Logger,
     private val keyManagementRepository: KeyManagementRepository,
+    private val messagesRepository: MessagesRepository,
+    private val metadataStorageRepository: MetadataStorageRepositoryInterface,
+    private val historyInterface: HistoryInterface,
     private val subscribeStorageRepository: SubscribeStorageRepository,
     private val jsonRpcInteractor: JsonRpcInteractorInterface,
     _moshi: Moshi.Builder,
@@ -31,8 +40,8 @@ internal class OnSubscriptionUpdateEventUseCase(
 
                 keyManagementRepository.setKey(SymmetricKey(syncedSubscription.symKey), activeSubscription.pushTopic.value)
 
-                runCatching {
-                    with(activeSubscription) {
+                with(activeSubscription) {
+                    runCatching {
                         subscribeStorageRepository.insertOrAbortActiveSubscription(
                             account.value,
                             expiry.seconds,
@@ -43,18 +52,42 @@ internal class OnSubscriptionUpdateEventUseCase(
                             pushTopic.value,
                             null,
                         )
-                    }
-                }.fold(
-                    onSuccess = { jsonRpcInteractor.subscribe(activeSubscription.pushTopic) { error -> logger.error(error) } },
-                    onFailure = { error -> logger.error("Failed to insert Synced Subscription: $error") }
-                )
+                    }.mapCatching {
+                        metadataStorageRepository.insertOrAbortMetadata(topic = pushTopic, appMetaData = dappMetaData!!, appMetaDataType = AppMetaDataType.PEER)
+                    }.fold(
+                        onFailure = { error -> logger.error("Failed to insert Synced Subscription: $error") },
+                        onSuccess = {
+                            jsonRpcInteractor.subscribe(pushTopic) { error -> logger.error(error) }
+                            getPushMessagesFromHistory(pushTopic) { messagesCount ->
+                                if (messagesCount >= messagesBatchSize) logger.error("Fetched $messagesBatchSize for ${dappMetaData!!.url}")
+                                else logger.log("Fetched $messagesCount for ${dappMetaData!!.url}")
+
+                                Timber.d("Sync getPushMessagesFromHistory: $pushTopic")
+                            }
+                        }
+                    )
+                }
             }
 
             is SyncUpdate.SyncDelete -> {
                 val pushTopic = update.key
                 subscribeStorageRepository.deleteSubscriptionByPushTopic(pushTopic)
+                messagesRepository.deleteMessagesByTopic(pushTopic)
                 jsonRpcInteractor.unsubscribe(Topic(pushTopic))
+                Timber.d("Sync deleting: $pushTopic")
             }
         }
+    }
+
+    private suspend fun getPushMessagesFromHistory(pushTopic: Topic, onSuccess: (Int) -> Unit) {
+        historyInterface.getMessages(
+            MessagesParams(pushTopic.value, null, messagesBatchSize, null),
+            onError = { error -> logger.error(error.throwable) },
+            onSuccess = { onSuccess(it.size) }
+        )
+    }
+
+    private companion object {
+        const val messagesBatchSize = 200L
     }
 }
