84113a1ca188cbeb60bff3401db9c8fff7532fc3
kubel <jakubwitczak6@gmail.com>
2023-07-13 12:41:34 +0200

    Clean up

--- androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/json_rpc/domain/JsonRpcInteractor.kt
+++ androidCore/sdk/src/main/kotlin/com/walletconnect/android/internal/common/json_rpc/domain/JsonRpcInteractor.kt
@@ -86,9 +86,6 @@ internal class JsonRpcInteractor(
         }
 
         val requestJson = serializer.serialize(payload) ?: return onFailure(IllegalStateException("JsonRpcInteractor: Unknown result params"))
-
-        println("kobe; Request: $requestJson")
-
         if (jsonRpcHistory.setRequest(payload.id, topic, payload.method, requestJson)) {
             val encryptedRequest = chaChaPolyCodec.encrypt(topic, requestJson, envelopeType, participants)
 
@@ -118,9 +115,6 @@ internal class JsonRpcInteractor(
 
         val jsonResponseDO = response.toJsonRpcResponse()
         val responseJson = serializer.serialize(jsonResponseDO) ?: return onFailure(IllegalStateException("JsonRpcInteractor: Unknown result params"))
-
-        println("kobe; Response: $responseJson")
-
         val encryptedResponse = chaChaPolyCodec.encrypt(topic, responseJson, envelopeType, participants)
         relay.publish(topic.value, encryptedResponse, params.toRelay()) { result ->
             result.fold(
@@ -319,8 +313,6 @@ internal class JsonRpcInteractor(
                         handleError("ManSub: ${e.stackTraceToString()}")
                         String.Empty
                     }
-
-                    println("kobe; Message: $message")
 
                     Pair(message, topic)
                 }.collect { (decryptedMessage, topic) ->
