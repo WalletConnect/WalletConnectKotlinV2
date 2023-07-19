780edc1aa1780b0e9780b55c7e49cf26d0402d71
Kacper <kacperdabrowski94@gmail.com>
2023-07-19 09:21:03 +0200

    Remove useless dependecies

--- sample/modals/build.gradle.kts
+++ sample/modals/build.gradle.kts
@@ -53,9 +53,6 @@ android {
 dependencies {
     implementation(project(":sample:common"))
 
-    implementation ("io.insert-koin:koin-androidx-compose:3.4.3")
-    implementation ("io.coil-kt:coil-compose:2.3.0")
-
     compose()
     accompanist()
     appCompat()
@@ -63,7 +60,6 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
 
     debugImplementation(project(":core:android"))
     debugImplementation(project(":product:web3modal"))
@@ -71,12 +67,4 @@ dependencies {
     releaseImplementation(platform("com.walletconnect:android-bom:$BOM_VERSION"))
     releaseImplementation("com.walletconnect:android-core")
     releaseImplementation(project(":product:web3modal"))
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
