e763e54030ab75fac47f55fa6cdd3e3b659c981c
Talha Ali <>
2023-07-26 07:34:35 -0500

    started Notify SDK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,26 +11,22 @@ val excludedDirs = listOf(
     "gradle",
     "docs",
     "licenses",
-    "signSDK",
-    "chatSDK",
-    "authSDK",
-    "coreSDK",
     "walletconnectv2"
 )
 // TODO: Add to rootModules when new module is added to the project root directory
 val rootModules = listOf("foundation")
 
-File(rootDir.path).listFiles { file -> file.isDirectory && file.name !in excludedDirs }?.forEach { childDir ->
-    if (childDir.name !in rootModules) {
-        childDir.listFiles { dir -> dir.isDirectory && dir.name !in excludedDirs}?.forEach { moduleDir ->
-            val module = ":${moduleDir.parentFile.name}:${moduleDir.name}"
-            include(module)
-            project(module).projectDir = moduleDir
-        }
-    } else {
-        include(":${childDir.name}")
-    }
-}
+//File(rootDir.path).listFiles { file -> file.isDirectory && file.name !in excludedDirs }?.forEach { childDir ->
+//    if (childDir.name !in rootModules) {
+//        childDir.listFiles { dir -> dir.isDirectory && dir.name !in excludedDirs}?.forEach { moduleDir ->
+//            val module = ":${moduleDir.parentFile.name}:${moduleDir.name}"
+//            include(module)
+//            project(module).projectDir = moduleDir
+//        }
+//    } else {
+//        include(":${childDir.name}")
+//    }
+//}
 
 pluginManagement {
     repositories {
@@ -39,3 +35,4 @@ pluginManagement {
         mavenCentral()
     }
 }
+include(":protocol:notify")
