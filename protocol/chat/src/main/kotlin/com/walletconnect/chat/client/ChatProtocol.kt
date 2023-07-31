86b5b468bd4fd55c50e8679f23730a16c4473833
kubel <jakubwitczak6@gmail.com>
2023-07-31 14:55:04 +0200

    Add init check on Chat and Push

--- protocol/chat/src/main/kotlin/com/walletconnect/chat/client/ChatProtocol.kt
+++ protocol/chat/src/main/kotlin/com/walletconnect/chat/client/ChatProtocol.kt
@@ -26,21 +26,25 @@ internal class ChatProtocol(private val koinApp: KoinApplication = wcKoinApp) : 
 
     @Throws(IllegalStateException::class)
     override fun initialize(init: Chat.Params.Init, onError: (Chat.Model.Error) -> Unit) {
-        try {
-            koinApp.run {
-                modules(
-                    jsonRpcModule(),
-                    storageModule(koinApp.koin.get<DatabaseConfig>().CHAT_SDK_DB_NAME),
-                    syncInChatModule(),
-                    engineModule(),
-                    commonModule()
-                )
-            }
+        if (!::chatEngine.isInitialized) {
+            try {
+                koinApp.run {
+                    modules(
+                        jsonRpcModule(),
+                        storageModule(koinApp.koin.get<DatabaseConfig>().CHAT_SDK_DB_NAME),
+                        syncInChatModule(),
+                        engineModule(),
+                        commonModule()
+                    )
+                }
 
-            chatEngine = koinApp.koin.get()
-            chatEngine.setup()
-        } catch (e: Exception) {
-            onError(Chat.Model.Error(e))
+                chatEngine = koinApp.koin.get()
+                chatEngine.setup()
+            } catch (e: Exception) {
+                onError(Chat.Model.Error(e))
+            }
+        } else {
+            onError(Chat.Model.Error(IllegalStateException("ChatClient already initialized")))
         }
     }
 
