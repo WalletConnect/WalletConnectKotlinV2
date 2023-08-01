d0b2bd65ea9c527fc8d74c003e4d3a8bb62b9a34
szymon <sz.rzad@gmail.com>
2023-08-01 14:52:40 +0200

    feat: Change data type

--- core/android/src/main/kotlin/com/walletconnect/android/history/network/model/messages/MessagesParams.kt
+++ core/android/src/main/kotlin/com/walletconnect/android/history/network/model/messages/MessagesParams.kt
@@ -5,13 +5,13 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class MessagesParams(
     val topic: String,
-    val originId: Long?,
-    val messageCount: Long?,
+    val originId: String?,
+    val messageCount: Int?,
     val direction: Direction?,
 ) {
     fun toQueryMap(): Map<String, String> {
         return mutableMapOf("topic" to topic).apply {
-            if (originId != null) this["originId"] = originId.toString()
+            if (originId != null) this["originId"] = originId
             if (messageCount != null) this["messageCount"] = messageCount.toString()
             if (direction != null) this["direction"] = direction.toString()
         }
