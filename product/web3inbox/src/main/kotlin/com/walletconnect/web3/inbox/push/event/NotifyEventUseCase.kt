17272627196516872e9c46f82a1b01335aa6c719
Talha Ali <>
2023-08-01 23:04:03 -0500

    Renamed PushEventUseCase.kt to NotifyEventUseCase

--- product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/NotifyEventUseCase.kt
+++ product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/NotifyEventUseCase.kt
@@ -3,7 +3,7 @@ package com.walletconnect.web3.inbox.push.event
 import com.walletconnect.web3.inbox.common.proxy.PushProxyInteractor
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxRPC
 
-internal abstract class PushEventUseCase<T : Any>(val proxyInteractor: PushProxyInteractor) {
+internal abstract class NotifyEventUseCase<T : Any>(val proxyInteractor: PushProxyInteractor) {
     abstract operator fun invoke(model: T)
     fun <T : Web3InboxRPC.Call.Push> call(call: T) = proxyInteractor.call(call)
 }
