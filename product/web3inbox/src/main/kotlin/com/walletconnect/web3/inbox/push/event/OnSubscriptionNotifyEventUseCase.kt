5d00271d6d11e662f8702da3f18705c2c1919cd1
Talha Ali <>
2023-08-02 22:09:55 -0500

    Updated OnSubscriptionNotifyEventUseCase to use Notify

--- product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/OnSubscriptionNotifyEventUseCase.kt
+++ product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/OnSubscriptionNotifyEventUseCase.kt
@@ -2,17 +2,16 @@ package com.walletconnect.web3.inbox.push.event
 
 import com.walletconnect.android.Core
 import com.walletconnect.push.client.Push
-import com.walletconnect.web3.inbox.common.proxy.PushProxyInteractor
+import com.walletconnect.web3.inbox.common.proxy.NotifyProxyInteractor
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxParams
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxRPC
 
-
-internal class OnSubscriptionPushEventUseCase(
-    proxyInteractor: PushProxyInteractor,
-) : PushEventUseCase<Push.Event.Subscription>(proxyInteractor) {
+internal class OnSubscriptionNotifyEventUseCase(
+    proxyInteractor: NotifyProxyInteractor,
+) : NotifyEventUseCase<Push.Event.Subscription>(proxyInteractor) {
 
     override fun invoke(model: Push.Event.Subscription) =
-        call(Web3InboxRPC.Call.Push.Subscription(params = model.toParams()))
+        call(Web3InboxRPC.Call.Notify.Subscription(params = model.toParams()))
 
     private fun Push.Event.Subscription.toParams() = when (this) {
         is Push.Event.Subscription.Error -> this.toParams()
@@ -20,10 +19,10 @@ internal class OnSubscriptionPushEventUseCase(
     }
 
     private fun Push.Event.Subscription.Error.toParams() =
-        Web3InboxParams.Call.Push.Subscription.ErrorParams(id, reason)
+        Web3InboxParams.Call.Notify.Subscription.ErrorParams(id, reason)
 
     private fun Push.Event.Subscription.Result.toParams() =
-        Web3InboxParams.Call.Push.Subscription.ResultParams(subscription.toParams())
+        Web3InboxParams.Call.Notify.Subscription.ResultParams(subscription.toParams())
 
     private fun Push.Model.Subscription.toParams() =
         Web3InboxParams.SubscriptionParams(topic, account, relay.toParams(), metadata.toParams(), scope.toParams(), expiry)
