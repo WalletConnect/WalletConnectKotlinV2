1059ba87fda962502d9ab411f6d8171ff60f524f
Talha Ali <>
2023-08-02 22:27:29 -0500

    Updated OnMessageNotifyEventUseCase to use Notify params

--- product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/OnMessageNotifyEventUseCase.kt
+++ product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/OnMessageNotifyEventUseCase.kt
@@ -1,22 +1,22 @@
 package com.walletconnect.web3.inbox.push.event
 
-import com.walletconnect.push.client.Push
+import com.walletconnect.notify.client.Notify
 import com.walletconnect.web3.inbox.common.proxy.NotifyProxyInteractor
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxParams
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxRPC
 
 internal class OnMessageNotifyEventUseCase(
     proxyInteractor: NotifyProxyInteractor,
-) : NotifyEventUseCase<Push.Event.Message>(proxyInteractor) {
+) : NotifyEventUseCase<Notify.Event.Message>(proxyInteractor) {
 
-    override fun invoke(model: Push.Event.Message) =
+    override fun invoke(model: Notify.Event.Message) =
         call(Web3InboxRPC.Call.Notify.Message(params = model.toParams()))
 
-    private fun Push.Event.Message.toParams() =
+    private fun Notify.Event.Message.toParams() =
         Web3InboxParams.Call.Notify.MessageParams(message.toParams())
 
-    private fun Push.Model.MessageRecord.toParams() =
+    private fun Notify.Model.MessageRecord.toParams() =
         Web3InboxParams.Call.Notify.MessageParams.MessageRecord(id, topic, publishedAt, message.toParams())
 
-    private fun Push.Model.Message.toParams() = Web3InboxParams.Call.Notify.MessageParams.Message(title, body, icon, url)
+    private fun Notify.Model.Message.toParams() = Web3InboxParams.Call.Notify.MessageParams.Message(title, body, icon, url)
 }
