5aefcebe8ef939fcc59ad89eee54a3ab64c97252
Talha Ali <>
2023-08-02 22:28:07 -0500

    Updated OnDeleteNotifyEventUseCase to use Notify params

--- product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/OnDeleteNotifyEventUseCase.kt
+++ product/web3inbox/src/main/kotlin/com/walletconnect/web3/inbox/push/event/OnDeleteNotifyEventUseCase.kt
@@ -1,17 +1,17 @@
 package com.walletconnect.web3.inbox.push.event
 
-import com.walletconnect.push.client.Push
+import com.walletconnect.notify.client.Notify
 import com.walletconnect.web3.inbox.common.proxy.NotifyProxyInteractor
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxParams
 import com.walletconnect.web3.inbox.json_rpc.Web3InboxRPC
 
 internal class OnDeleteNotifyEventUseCase(
     proxyInteractor: NotifyProxyInteractor,
-) : NotifyEventUseCase<Push.Event.Delete>(proxyInteractor) {
+) : NotifyEventUseCase<Notify.Event.Delete>(proxyInteractor) {
 
-    override fun invoke(model: Push.Event.Delete) =
+    override fun invoke(model: Notify.Event.Delete) =
         call(Web3InboxRPC.Call.Notify.Delete(params = model.toParams()))
 
-    private fun Push.Event.Delete.toParams() =
+    private fun Notify.Event.Delete.toParams() =
         Web3InboxParams.Call.Notify.DeleteParams(topic)
 }
