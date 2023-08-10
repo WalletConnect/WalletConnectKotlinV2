fbe2cd95b371a9f4deb3b002b459371a0932f136
Talha Ali <>
2023-08-09 22:54:46 -0500

    Added Update JWT classes

---
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/data/jwt/update/EncodeUpdateRequestJwtUseCase.kt
@@ -0,0 +1,28 @@
+package com.walletconnect.notify.data.jwt.update
+
+import com.walletconnect.android.internal.common.jwt.did.EncodeDidJwtPayloadUseCase
+import com.walletconnect.android.internal.common.model.AccountId
+import com.walletconnect.foundation.common.model.PublicKey
+import com.walletconnect.foundation.util.jwt.encodeDidPkh
+import com.walletconnect.foundation.util.jwt.encodeEd25519DidKey
+
+class EncodeUpdateRequestJwtUseCase(
+    private val accountId: AccountId,
+    private val dappUrl: String,
+    private val authenticationKey: PublicKey,
+    private val scope: String
+) : EncodeDidJwtPayloadUseCase<UpdateRequestJwtClaim> {
+
+    override fun invoke(params: EncodeDidJwtPayloadUseCase.Params): UpdateRequestJwtClaim = with(params) {
+        UpdateRequestJwtClaim(
+            issuedAt = issuedAt,
+            expiration = expiration,
+            issuer = identityKeyDidKey,
+            keyserverUrl = keyserverUrl,
+            audience = encodeEd25519DidKey(authenticationKey.keyAsBytes),
+            subject = encodeDidPkh(accountId.value),
+            dappUrl = dappUrl,
+            scope = scope,
+        )
+    }
+}
