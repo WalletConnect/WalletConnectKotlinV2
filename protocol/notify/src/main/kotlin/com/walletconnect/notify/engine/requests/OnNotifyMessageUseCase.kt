38fd35753fef3b9c55209db471e91c84eaa0650e
Talha Ali <>
2023-08-09 23:04:03 -0500

    Updated OnNotifyMessageUseCase with Jwt logic

--- protocol/notify/src/main/kotlin/com/walletconnect/notify/engine/requests/OnNotifyMessageUseCase.kt
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/engine/requests/OnNotifyMessageUseCase.kt
@@ -2,8 +2,9 @@
 
 package com.walletconnect.notify.engine.requests
 
+import com.squareup.moshi.Moshi
+import com.walletconnect.android.internal.common.crypto.sha256
 import com.walletconnect.android.internal.common.exception.Uncategorized
-import com.walletconnect.android.internal.common.jwt.did.encodeDidJwt
 import com.walletconnect.android.internal.common.jwt.did.extractVerifiedDidJwtClaims
 import com.walletconnect.android.internal.common.model.IrnParams
 import com.walletconnect.android.internal.common.model.Tags
@@ -11,12 +12,14 @@ import com.walletconnect.android.internal.common.model.WCRequest
 import com.walletconnect.android.internal.common.model.params.NotifyParams
 import com.walletconnect.android.internal.common.model.type.EngineEvent
 import com.walletconnect.android.internal.common.model.type.JsonRpcInteractorInterface
-import com.walletconnect.android.internal.utils.DAY_IN_SECONDS
+import com.walletconnect.android.internal.utils.MONTH_IN_SECONDS
 import com.walletconnect.foundation.common.model.Ttl
 import com.walletconnect.notify.common.model.NotifyMessage
 import com.walletconnect.notify.common.model.NotifyRecord
 import com.walletconnect.notify.data.jwt.message.MessageRequestJwtClaim
 import com.walletconnect.notify.data.storage.MessagesRepository
+import com.walletconnect.notify.data.storage.SubscriptionRepository
+import com.walletconnect.notify.engine.domain.RegisterIdentityAndReturnDidJwtInteractor
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -25,50 +28,69 @@ import kotlinx.coroutines.supervisorScope
 internal class OnNotifyMessageUseCase(
     private val jsonRpcInteractor: JsonRpcInteractorInterface,
     private val messagesRepository: MessagesRepository,
+    private val subscriptionRepository: SubscriptionRepository,
+    private val registerIdentityAndReturnDidJwt: RegisterIdentityAndReturnDidJwtInteractor,
+    private val _moshi: Moshi.Builder,
 ) {
     private val _events: MutableSharedFlow<EngineEvent> = MutableSharedFlow()
     val events: SharedFlow<EngineEvent> = _events.asSharedFlow()
 
     suspend operator fun invoke(request: WCRequest, params: NotifyParams.MessageParams) = supervisorScope {
-        val irnParams = IrnParams(Tags.NOTIFY_MESSAGE_RESPONSE, Ttl(DAY_IN_SECONDS))
+        extractVerifiedDidJwtClaims<MessageRequestJwtClaim>(params.messageAuth).onSuccess { messageJwt ->
+            messagesRepository.insertMessage(
+                requestId = request.id,
+                topic = request.topic.value,
+                publishedAt = request.publishedAt,
+                title = messageJwt.message.title,
+                body = messageJwt.message.body,
+                icon = messageJwt.message.icon,
+                url = messageJwt.message.url,
+                type = messageJwt.message.type
+            )
 
-        try {
-            extractVerifiedDidJwtClaims<MessageRequestJwtClaim>(params.messageAuth).mapCatching { messageJwt ->
-                messagesRepository.insertMessage(
-                    requestId = request.id,
-                    topic = request.topic.value,
-                    publishedAt = request.publishedAt,
+            val notifyRecord = NotifyRecord(
+                id = request.id,
+                topic = request.topic.value,
+                publishedAt = request.publishedAt,
+                notifyMessage = NotifyMessage(
                     title = messageJwt.message.title,
                     body = messageJwt.message.body,
                     icon = messageJwt.message.icon,
                     url = messageJwt.message.url,
                     type = messageJwt.message.type
                 )
+            )
+            _events.emit(notifyRecord)
+        }.mapCatching { jwtMessage ->
+            val stringifiedMessage = _moshi.build().adapter(MessageRequestJwtClaim.Message::class.java).toJson(jwtMessage.message)
+            val messageHash = sha256(stringifiedMessage.encodeToByteArray())
+            val activeSubscription = subscriptionRepository.getActiveSubscriptionByNotifyTopic(request.topic.value) ?: throw IllegalStateException("No active subscription for topic: ${request.topic.value}")
+            val messageReceiptJwt = registerIdentityAndReturnDidJwt.messageReceipt(
+                account = activeSubscription.account,
+                metadataUrl = activeSubscription.dappMetaData!!.url,
+                authenticationKey = activeSubscription.authenticationPublicKey,
+                messageHash = messageHash,
+                onFailure = { e ->
+                    throw e
+                }
+            ).getOrThrow()
 
-                val notifyRecord = NotifyRecord(
-                    id = request.id,
-                    topic = request.topic.value,
-                    publishedAt = request.publishedAt,
-                    notifyMessage = NotifyMessage(
-                        title = messageJwt.message.title,
-                        body = messageJwt.message.body,
-                        icon = messageJwt.message.icon,
-                        url = messageJwt.message.url,
-                        type = messageJwt.message.type
-                    )
-                )
-                _events.emit(notifyRecord)
-            }.mapCatching {
+            val messageReceiptParams = NotifyParams.MessageReceiptParams(receiptAuth = messageReceiptJwt.value)
+            val irnParams = IrnParams(Tags.NOTIFY_MESSAGE_RESPONSE, Ttl(MONTH_IN_SECONDS))
 
-                // TODO: RespondWithParams with the receiptAuth
-                //jsonRpcInteractor.respondWithParams(request.id, request.topic, )
-                jsonRpcInteractor.respondWithSuccess(request, irnParams)
-            }.getOrThrow()
-        } catch (e: Exception) {
+            jsonRpcInteractor.respondWithParams(
+                request.id,
+                request.topic,
+                messageReceiptParams,
+                irnParams
+            ) {
+                throw it
+            }
+        }.getOrElse { e ->
             jsonRpcInteractor.respondWithError(
                 request,
                 Uncategorized.GenericError("Cannot handle the notify message: ${e.message}, topic: ${request.topic}"),
-                irnParams
+                IrnParams(Tags.NOTIFY_MESSAGE_RESPONSE, Ttl(MONTH_IN_SECONDS))
             )
         }
     }
