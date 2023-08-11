aa85e31c658c89f4aa59e6530d2f996884122723
Talha Ali <>
2023-08-10 23:02:19 -0500

    made NotifySyncStores internal

--- protocol/notify/src/main/kotlin/com/walletconnect/notify/engine/sync/NotifySyncStores.kt
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/engine/sync/NotifySyncStores.kt
@@ -1,7 +1,9 @@
+@file:JvmSynthetic
+
 package com.walletconnect.notify.engine.sync
 
 private const val NOTIFY_STORE_PREFIX = "com.walletconnect.notify."
 
-enum class NotifySyncStores(val value: String) {
+internal enum class NotifySyncStores(val value: String) {
     NOTIFY_SUBSCRIPTION(NOTIFY_STORE_PREFIX + "notifySubscription"),
 }
