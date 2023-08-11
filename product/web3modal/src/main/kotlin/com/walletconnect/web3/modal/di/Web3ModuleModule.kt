b246fda8de41b715c5c0e1fe40c64e534ccec17c
Kacper <kacperdabrowski94@gmail.com>
2023-08-11 17:15:56 +0200

    Remove unused files

--- product/web3modal/src/main/kotlin/com/walletconnect/web3/modal/di/Web3ModuleModule.kt
+++ product/web3modal/src/main/kotlin/com/walletconnect/web3/modal/di/Web3ModuleModule.kt
@@ -1,21 +1,7 @@
 package com.walletconnect.web3.modal.di
 
-import com.squareup.moshi.Moshi
-import com.walletconnect.android.internal.common.di.AndroidCommonDITags
-import com.walletconnect.web3.modal.domain.configuration.EncodedStringAdapter
-import com.walletconnect.web3.modal.domain.configuration.configAdapter
-
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-internal const val WEB3MODAL_MOSHI = "web3modal_moshi"
-
 internal fun web3ModalModule() = module {
-
-    single(named(WEB3MODAL_MOSHI)) {
-        get<Moshi.Builder>(named(AndroidCommonDITags.MOSHI))
-            .add(configAdapter())
-            .add(EncodedStringAdapter())
-            .build()
-    }
+        // TODO Add Local storage for modal here.
 }
