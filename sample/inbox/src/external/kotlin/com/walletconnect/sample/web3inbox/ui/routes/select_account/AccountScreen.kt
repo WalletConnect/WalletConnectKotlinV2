72b07423ebfaf7c7fb5b03006903751bdabfb922
Talha Ali <>
2023-09-11 18:06:35 +0200

    removed random eth account logic from external AccountScreen

--- sample/inbox/src/external/kotlin/com/walletconnect/sample/web3inbox/ui/routes/select_account/AccountScreen.kt
+++ sample/inbox/src/external/kotlin/com/walletconnect/sample/web3inbox/ui/routes/select_account/AccountScreen.kt
@@ -7,17 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.walletconnect.sample.web3inbox.domain.EthAccount
 import com.walletconnect.wcmodal.ui.openWalletConnectModal
 
 @Composable
 fun AccountScreen(navController: NavController) {
-    val context = LocalContext.current
-    val random = EthAccount.Random(context)
-
     Box {
         Column(
             verticalArrangement = Arrangement.Center, modifier = Modifier
