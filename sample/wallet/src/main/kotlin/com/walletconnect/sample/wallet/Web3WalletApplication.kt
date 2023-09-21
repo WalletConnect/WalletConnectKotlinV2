6f27ab98a1bd37662bc90208d4607e02b79728b2
szymon <sz.rzad@gmail.com>
2023-09-21 11:09:40 +0200

    chore: Enable MixPanel for sample wallet

--- sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/Web3WalletApplication.kt
+++ sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/Web3WalletApplication.kt
@@ -7,9 +7,11 @@ import android.util.Log
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
+import com.mixpanel.android.mpmetrics.MixpanelAPI
 import com.pandulapeter.beagle.modules.DividerModule
 import com.pandulapeter.beagle.modules.HeaderModule
 import com.pandulapeter.beagle.modules.PaddingModule
+import com.pandulapeter.beagle.modules.TextInputModule
 import com.pandulapeter.beagle.modules.TextModule
 import com.walletconnect.android.Core
 import com.walletconnect.android.CoreClient
@@ -24,6 +26,7 @@ import com.walletconnect.sample.common.RELAY_URL
 import com.walletconnect.sample.common.initBeagle
 import com.walletconnect.sample.common.tag
 import com.walletconnect.sample.wallet.domain.EthAccountDelegate
+import com.walletconnect.sample.wallet.domain.mixPanel
 import com.walletconnect.sample.wallet.domain.toEthAddress
 import com.walletconnect.sample.wallet.ui.state.ConnectionState
 import com.walletconnect.sample.wallet.ui.state.connectionStateFlow
@@ -35,6 +38,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import com.walletconnect.sample.common.BuildConfig as CommonBuildConfig
 
 class Web3WalletApplication : Application() {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
@@ -109,42 +113,42 @@ class Web3WalletApplication : Application() {
             TextModule(text = CoreClient.Echo.clientId) {
                 (getSystemService(CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(ClipData.newPlainText("ClientId", CoreClient.Echo.clientId))
             },
-//            DividerModule(),
-//            TextInputModule(
-//                text = "Import Private Key",
-//                validator = { text ->
-//                    !text.startsWith("0x") && text.length == 64
-//                },
-//                onValueChanged = { text ->
-//                    EthAccountDelegate.privateKey = text
-//
-//
-//                    NotifyClient.register(
-//                        params = Notify.Params.Registration(
-//                            with(EthAccountDelegate) { account.toEthAddress() },
-//                            isLimited = false,
-//                            domain = BuildConfig.APPLICATION_ID,
-//                            onSign = { message -> CacaoSigner.sign(message, EthAccountDelegate.privateKey.hexToBytes(), SignatureType.EIP191) }
-//                        ),
-//                        onSuccess = {
-//                            Log.e(tag(this), "Register Success")
-//                        },
-//                        onError = {
-//                            Log.e(tag(this), it.throwable.stackTraceToString())
-//                        }
-//                    )
-//                }
-//            )
+            DividerModule(),
+            TextInputModule(
+                text = "Import Private Key",
+                validator = { text ->
+                    !text.startsWith("0x") && text.length == 64
+                },
+                onValueChanged = { text ->
+                    EthAccountDelegate.privateKey = text
+
+
+                    NotifyClient.register(
+                        params = Notify.Params.Registration(
+                            with(EthAccountDelegate) { account.toEthAddress() },
+                            isLimited = false,
+                            domain = BuildConfig.APPLICATION_ID,
+                            onSign = { message -> CacaoSigner.sign(message, EthAccountDelegate.privateKey.hexToBytes(), SignatureType.EIP191) }
+                        ),
+                        onSuccess = {
+                            Log.e(tag(this), "Register Success")
+                        },
+                        onError = {
+                            Log.e(tag(this), it.throwable.stackTraceToString())
+                        }
+                    )
+                }
+            )
         )
 
-//        mixPanel = MixpanelAPI.getInstance(this, CommonBuildConfig.MIX_PANEL, true).apply {
-//            identify(CoreClient.Echo.clientId)
-//            people.set("\$name", with(EthAccountDelegate) { account.toEthAddress() })
-//        }
+        mixPanel = MixpanelAPI.getInstance(this, CommonBuildConfig.MIX_PANEL, true).apply {
+            identify(CoreClient.Echo.clientId)
+            people.set("\$name", with(EthAccountDelegate) { account.toEthAddress() })
+        }
 
         wcKoinApp.koin.get<Timber.Forest>().plant(object : Timber.Tree() {
             override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-//                mixPanel.track(message)
+                mixPanel.track(message)
             }
         })
 
