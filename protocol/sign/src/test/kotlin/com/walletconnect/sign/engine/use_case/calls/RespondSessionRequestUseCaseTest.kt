d52c147bbe29a1a7a30cc0096e851cfb1c7951ef
Talha Ali <>
2023-10-02 23:03:42 -0500

    made RespondSessionRequestUseCase return exceptions in onError instead of throwing and added unit test

---
+++ protocol/sign/src/test/kotlin/com/walletconnect/sign/engine/use_case/calls/RespondSessionRequestUseCaseTest.kt
@@ -0,0 +1,56 @@
+package com.walletconnect.sign.engine.use_case.calls
+
+import com.walletconnect.android.internal.common.exception.CannotFindSequenceForTopic
+import com.walletconnect.android.internal.common.model.type.JsonRpcInteractorInterface
+import com.walletconnect.android.internal.common.storage.VerifyContextStorageRepository
+import com.walletconnect.foundation.util.Logger
+import com.walletconnect.sign.json_rpc.domain.GetPendingJsonRpcHistoryEntryByIdUseCase
+import com.walletconnect.sign.storage.sequence.SessionStorageRepository
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class RespondSessionRequestUseCaseTest {
+    private val jsonRpcInteractor = mockk<JsonRpcInteractorInterface>()
+    private val sessionStorageRepository = mockk<SessionStorageRepository>()
+    private val getPendingJsonRpcHistoryEntryByIdUseCase = mockk<GetPendingJsonRpcHistoryEntryByIdUseCase>()
+    private val logger = mockk<Logger>()
+    private val verifyContextStorageRepository = mockk<VerifyContextStorageRepository>()
+    private val respondSessionRequestUseCase = RespondSessionRequestUseCase(
+        jsonRpcInteractor,
+        sessionStorageRepository,
+        getPendingJsonRpcHistoryEntryByIdUseCase,
+        logger,
+        verifyContextStorageRepository
+    )
+
+    @Before
+    fun setUp() {
+        every { logger.error(any() as String) } answers { }
+        every { logger.error(any() as Exception) } answers { }
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun `onFailure is called when sessionStorageRepository isSessionValid is false`() = runTest {
+        every { sessionStorageRepository.isSessionValid(any()) } returns false
+
+        respondSessionRequestUseCase.respondSessionRequest(
+            topic = "topic",
+            jsonRpcResponse = mockk(),
+            onSuccess = {
+                Assert.fail("onSuccess should not be called since should have validation failed")
+            },
+            onFailure = { error ->
+                Assert.assertSame(CannotFindSequenceForTopic::class, error::class)
+            }
+        )
+    }
+}
