7ab7b44e827f2e649d2a72e292cb739ba90c0e53
Talha Ali <>
2023-10-03 09:11:38 -0500

    Removed teardown in SessionUpdateUseCaseTest

--- protocol/sign/src/test/kotlin/com/walletconnect/sign/engine/use_case/calls/SessionUpdateUseCaseTest.kt
+++ protocol/sign/src/test/kotlin/com/walletconnect/sign/engine/use_case/calls/SessionUpdateUseCaseTest.kt
@@ -7,7 +7,6 @@ import com.walletconnect.sign.storage.sequence.SessionStorageRepository
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.After
 import org.junit.Assert.assertSame
 import org.junit.Assert.fail
 import org.junit.Before
@@ -23,10 +22,6 @@ class SessionUpdateUseCaseTest {
     fun setUp() {
         every { logger.error(any() as String) } answers { }
         every { logger.error(any() as Exception) } answers { }
-    }
-
-    @After
-    fun tearDown() {
     }
 
     @Test
