3b2629bbc06c6ece0dbad638583e17d6fe342315
Talha Ali <>
2023-10-02 22:54:02 -0500

    made EmitEventUseCase return exceptions in onError instead of throwing and added unit test

---
+++ protocol/sign/src/test/kotlin/com/walletconnect/sign/engine/use_case/calls/EmitEventUseCaseTest.kt
@@ -0,0 +1,49 @@
+package com.walletconnect.sign.engine.use_case.calls
+
+import com.walletconnect.android.internal.common.exception.CannotFindSequenceForTopic
+import com.walletconnect.android.internal.common.model.type.JsonRpcInteractorInterface
+import com.walletconnect.foundation.util.Logger
+import com.walletconnect.sign.engine.model.EngineDO
+import com.walletconnect.sign.storage.sequence.SessionStorageRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class EmitEventUseCaseTest {
+    private val jsonRpcInteractor = mockk<JsonRpcInteractorInterface>()
+    private val sessionStorageRepository = mockk<SessionStorageRepository>()
+    private val logger = mockk<Logger>()
+    private val emitEventUseCase = EmitEventUseCase(jsonRpcInteractor, sessionStorageRepository, logger)
+
+    @Before
+    fun setUp() {
+        every { logger.error(any() as String) } answers { }
+        every { logger.error(any() as Exception) } answers { }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `onFailure is called when sessionStorageRepository isSessionValid is false`() = runTest {
+        every { sessionStorageRepository.isSessionValid(any()) } returns false
+
+        emitEventUseCase.emit(
+            topic = "topic",
+            event = EngineDO.Event("name", "data", "chainId"),
+            onSuccess = {
+                Assert.fail("onSuccess should not be called since should have validation failed")
+            },
+            onFailure = { error ->
+                Assert.assertSame(CannotFindSequenceForTopic::class, error::class)
+            }
+        )
+    }
+}
