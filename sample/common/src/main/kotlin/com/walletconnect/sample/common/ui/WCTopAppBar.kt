6df04cbdcf40256c2db311a46045eb0e5c9aa330
szymon <sz.rzad@gmail.com>
2023-10-04 12:20:32 +0200

    feat: Change ripple effect modifier

--- sample/common/src/main/kotlin/com/walletconnect/sample/common/ui/WCTopAppBar.kt
+++ sample/common/src/main/kotlin/com/walletconnect/sample/common/ui/WCTopAppBar.kt
@@ -3,21 +3,24 @@ package com.walletconnect.sample.common.ui
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -215,15 +218,15 @@ fun WCTopAppBar(
         Spacer(modifier = Modifier.weight(1f))
         firstIcon?.let {
             Image(
-                modifier = Modifier.largerCircularClickable(48.dp) { onFirstIconClick?.invoke() },
+                modifier = Modifier.largerCircularClickable(24.dp) { onFirstIconClick?.invoke() },
                 painter = painterResource(id = firstIcon),
                 contentDescription = null,
             )
-            Spacer(modifier = Modifier.width(8.dp))
         }
         secondIcon?.let {
+            Spacer(modifier = Modifier.width(24.dp))
             Image(
-                modifier = Modifier.largerCircularClickable(48.dp) { onSecondIconClick?.invoke() },
+                modifier = Modifier.largerCircularClickable(24.dp) { onSecondIconClick?.invoke() },
                 painter = painterResource(id = secondIcon),
                 contentDescription = null,
             )
@@ -231,8 +234,12 @@ fun WCTopAppBar(
     }
 }
 
-fun Modifier.largerCircularClickable(size: Dp, onClick: () -> Unit) = this.then(Modifier
-    .size(size)
-    .clip(RoundedCornerShape(size / 2))
-    .clickable { onClick() }
-    .padding(size / 6))
+fun Modifier.largerCircularClickable(radius: Dp, onClick: () -> Unit) = composed {
+    this.then(
+        Modifier.clickable(
+            indication = rememberRipple(bounded = false, radius = radius),
+            interactionSource = remember { MutableInteractionSource() },
+            onClick = onClick
+        )
+    )
+}
