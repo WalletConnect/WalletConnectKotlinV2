b8c80a647ecb02ab6794fa5020ea1a62aba819f2
szymon <sz.rzad@gmail.com>
2023-10-11 12:41:22 +0200

    feat: remove async launch

--- sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/composable_routes/settings/SettingsViewModel.kt
+++ sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/composable_routes/settings/SettingsViewModel.kt
@@ -1,14 +1,12 @@
 package com.walletconnect.sample.wallet.ui.routes.composable_routes.settings
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.messaging.FirebaseMessaging
 import com.walletconnect.android.CoreClient
 import com.walletconnect.sample.wallet.domain.EthAccountDelegate
 import com.walletconnect.sample.wallet.domain.toEthAddress
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 class SettingsViewModel : ViewModel() {
     val caip10 = with(EthAccountDelegate) { account.toEthAddress() }
@@ -19,10 +17,8 @@ class SettingsViewModel : ViewModel() {
     val deviceToken = _deviceToken.asStateFlow()
 
     init {
-        viewModelScope.launch {
-            FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
-                _deviceToken.value = token
-            }
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+            _deviceToken.value = token
         }
     }
 }
