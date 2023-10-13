23ba370162662969730267d88d2d94ef266019e0
szymon <sz.rzad@gmail.com>
2023-10-13 14:29:01 +0200

    feat: Add id to NotificationScope

--- protocol/notify/src/main/kotlin/com/walletconnect/notify/common/model/NotificationScope.kt
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/common/model/NotificationScope.kt
@@ -5,15 +5,18 @@ package com.walletconnect.notify.common.model
 internal sealed class NotificationScope {
     abstract val name: String
     abstract val description: String
+    abstract val id: String
 
     data class Remote(
         override val name: String,
         override val description: String,
+        override val id: String,
     ) : NotificationScope()
 
     data class Cached(
         override val name: String,
         override val description: String,
+        override val id: String,
         val isSelected: Boolean,
     ) : NotificationScope()
 }
