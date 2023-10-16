2008b49ecbcd1ed8cb263e4618177dc3bee672c9
szymon <sz.rzad@gmail.com>
2023-10-16 14:05:07 +0200

    feat: Add app to jwt claims encoding

--- protocol/notify/src/main/kotlin/com/walletconnect/notify/data/jwt/watchSubscriptions/EncodeWatchSubscriptionsRequestJwtUseCase.kt
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/data/jwt/watchSubscriptions/EncodeWatchSubscriptionsRequestJwtUseCase.kt
@@ -6,11 +6,13 @@ import com.walletconnect.android.internal.common.jwt.did.EncodeDidJwtPayloadUseC
 import com.walletconnect.android.internal.common.model.AccountId
 import com.walletconnect.foundation.common.model.PublicKey
 import com.walletconnect.foundation.util.jwt.encodeDidPkh
+import com.walletconnect.foundation.util.jwt.encodeDidWeb
 import com.walletconnect.foundation.util.jwt.encodeEd25519DidKey
 
 internal class EncodeWatchSubscriptionsRequestJwtUseCase(
     private val accountId: AccountId,
     private val authenticationKey: PublicKey,
+    private val appDomain: String?,
 ) : EncodeDidJwtPayloadUseCase<WatchSubscriptionsRequestJwtClaim> {
 
     override fun invoke(params: EncodeDidJwtPayloadUseCase.Params): WatchSubscriptionsRequestJwtClaim = with(params) {
@@ -21,6 +23,7 @@ internal class EncodeWatchSubscriptionsRequestJwtUseCase(
             keyserverUrl = keyserverUrl,
             audience = encodeEd25519DidKey(authenticationKey.keyAsBytes),
             subject = encodeDidPkh(accountId.value),
+            appDidWeb = appDomain?.let { encodeDidWeb(it) } ?: "null"
         )
     }
 }
