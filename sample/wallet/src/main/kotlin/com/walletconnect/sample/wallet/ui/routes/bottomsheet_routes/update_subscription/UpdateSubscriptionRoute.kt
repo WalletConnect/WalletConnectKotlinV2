334511d3272fbb5cde4fed8bf116d90f66b1ee73
szymon <sz.rzad@gmail.com>
2023-10-16 14:46:39 +0200

    fix: Add missing imports

--- sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/bottomsheet_routes/update_subscription/UpdateSubscriptionRoute.kt
+++ sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/ui/routes/bottomsheet_routes/update_subscription/UpdateSubscriptionRoute.kt
@@ -3,6 +3,7 @@
 package com.walletconnect.sample.wallet.ui.routes.bottomsheet_routes.update_subscription
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -29,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
