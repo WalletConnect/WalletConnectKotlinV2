4dd58262421b6888a79dc7001a7169d58986c855
szymon <sz.rzad@gmail.com>
2023-10-18 15:19:32 +0200

    chore: Remove jvmToolchain(11)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,6 @@ repositories {
     gradlePluginPortal()
 }
 
-kotlin.jvmToolchain(11)
-
 dependencies {
     implementation("com.android.tools.build:gradle:8.0.2") // when changing, remember to change version in Versions.kt in buildSrc module
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21") // when changing, remember to change version in Versions.kt in buildSrc module
