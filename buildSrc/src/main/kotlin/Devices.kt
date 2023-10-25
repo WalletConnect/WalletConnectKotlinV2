490d804461d7088ee4649bef5708d1346218f159
szymon <sz.rzad@gmail.com>
2023-10-25 13:22:54 +0200

    feat: Reduce managed devices to only Pixel 5

--- buildSrc/src/main/kotlin/Devices.kt
+++ buildSrc/src/main/kotlin/Devices.kt
@@ -3,24 +3,17 @@ import com.android.build.api.dsl.TestOptions
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.invoke
 
-
-// Device profiles taken from `$avdmanager list device` and tested locally for validity on 25/07/2023
-val devicesNames = listOf(
-    "Pixel 5", "Galaxy Nexus", "Nexus 10", "Nexus 4", "Nexus 5", "Nexus 5X", "Nexus 6", "Nexus 6P",
-    "Nexus 7", "Nexus 9", "Nexus One", "Nexus S", "Pixel", "Pixel 2", "Pixel 2 XL", "Pixel 3", "Pixel 3 XL",
-    "Pixel 3a", "Pixel 3a XL", "Pixel 4", "Pixel 4 XL", "Pixel 4a", "Pixel C", "Pixel XL"
-)
-
 fun TestOptions.registerManagedDevices() {
     managedDevices {
         devices {
-            devicesNames.first().let { deviceName ->
-                val taskSuffix = deviceName.replace(" ", "_")
-                create<ManagedVirtualDevice>("Google32_${taskSuffix}") {
-                    device = deviceName
-                    apiLevel = 32
-                    systemImageSource = "google"
-                }
+            val _deviceName = "Pixel 5"
+            val _apiLevel = 32
+            val _systemImageSource = "google"
+            val taskSuffix = "_" + _deviceName.replace(" ", "_")
+            create<ManagedVirtualDevice>("${_systemImageSource}${_apiLevel}${taskSuffix}") {
+                device = _deviceName
+                apiLevel = _apiLevel
+                systemImageSource = _systemImageSource
             }
         }
     }
