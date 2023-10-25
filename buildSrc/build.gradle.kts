e87ce5304c277e8fbdfe24c0a6990bb542f2abec
Talha Ali <>
2023-10-25 00:09:59 -0500

    created signing-config.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,11 @@ repositories {
     gradlePluginPortal()
 }
 
+kotlin.jvmToolchain(11)
+
 dependencies {
     implementation("com.android.tools.build:gradle:8.0.2") // when changing, remember to change version in Versions.kt in buildSrc module
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21") // when changing, remember to change version in Versions.kt in buildSrc module
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10") // when changing, remember to change version in Versions.kt in buildSrc module
+    implementation("com.google.firebase:firebase-appdistribution-gradle:4.0.0") // when changing, remember to change version in root build.gradle.kts
 }
