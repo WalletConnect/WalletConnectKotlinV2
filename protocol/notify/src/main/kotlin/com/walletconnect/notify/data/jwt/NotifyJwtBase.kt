03f0a13e0bd66b1b38120f34efac5d5d016e21a4
szymon <sz.rzad@gmail.com>
2023-10-26 16:36:59 +0200

    feat: Add validation to NotifyJWTBase

--- protocol/notify/src/main/kotlin/com/walletconnect/notify/data/jwt/NotifyJwtBase.kt
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/data/jwt/NotifyJwtBase.kt
@@ -4,8 +4,16 @@ package com.walletconnect.notify.data.jwt
 
 import com.walletconnect.foundation.util.jwt.JwtClaims
 
-internal interface NotifyJwtBase: JwtClaims {
+internal interface NotifyJwtBase : JwtClaims {
     val action: String
     val issuedAt: Long
     val expiration: Long
+
+    fun throwIdIssuedAtIsInvalid() {
+        if (issuedAt < System.currentTimeMillis()) throw IllegalArgumentException("Invalid issuedAt claim was $issuedAt instead of lower than ${System.currentTimeMillis()}")
+    }
+
+    fun throwExpirationAtIsInvalid() {
+        if (expiration > System.currentTimeMillis()) throw IllegalArgumentException("Invalid expiration claim was $expiration instead of greater than ${System.currentTimeMillis()}")
+    }
 }
