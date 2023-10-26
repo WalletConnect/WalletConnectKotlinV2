450ca7b89f904d1e0a5b32f76731a79f69cdd293
szymon <sz.rzad@gmail.com>
2023-10-26 16:36:27 +0200

    feat: Improve RegisteredAccount table

--- protocol/notify/src/main/kotlin/com/walletconnect/notify/data/storage/RegisteredAccountsRepository.kt
+++ protocol/notify/src/main/kotlin/com/walletconnect/notify/data/storage/RegisteredAccountsRepository.kt
@@ -4,6 +4,7 @@ package com.walletconnect.notify.data.storage
 
 import com.walletconnect.android.internal.common.model.AccountId
 import com.walletconnect.foundation.common.model.PublicKey
+import com.walletconnect.foundation.common.model.Topic
 import com.walletconnect.notify.common.model.RegisteredAccount
 import com.walletconnect.notify.common.storage.data.dao.RegisteredAccountsQueries
 import kotlinx.coroutines.Dispatchers
@@ -11,17 +12,20 @@ import kotlinx.coroutines.withContext
 
 internal class RegisteredAccountsRepository(private val registeredAccounts: RegisteredAccountsQueries) {
 
-    suspend fun insertOrIgnoreAccount(
-        accountId: AccountId,
-        publicIdentityKey: PublicKey,
-        isLimited: Boolean,
-        appDomain: String?
-    ) = withContext(Dispatchers.IO) {
+    suspend fun insertOrIgnoreAccount(accountId: AccountId, publicIdentityKey: PublicKey, isLimited: Boolean, appDomain: String?) = withContext(Dispatchers.IO) {
         registeredAccounts.insertOrIgnoreAccount(accountId.value, publicIdentityKey.keyAsHex, isLimited, appDomain)
+    }
+
+    suspend fun updateNotifyServerData(accountId: AccountId, notifyServerWatchTopic: Topic, notifyServerAuthenticationKey: PublicKey) = withContext(Dispatchers.IO) {
+        registeredAccounts.updateNotifyServerData(accountId.value, notifyServerWatchTopic.value, notifyServerAuthenticationKey.keyAsHex)
     }
 
     suspend fun getAccountByAccountId(accountId: String): RegisteredAccount = withContext(Dispatchers.IO) {
         registeredAccounts.getAccountByAccountId(accountId, ::toRegisterAccount).executeAsOne()
+    }
+
+    suspend fun getAccountByIdentityKey(identityPublicKey: String): RegisteredAccount = withContext(Dispatchers.IO) {
+        registeredAccounts.getAccountByIdentityKey(identityPublicKey, ::toRegisterAccount).executeAsOne()
     }
 
     suspend fun getAllAccounts(): List<RegisteredAccount> = withContext(Dispatchers.IO) {
@@ -33,9 +37,7 @@ internal class RegisteredAccountsRepository(private val registeredAccounts: Regi
     }
 
     private fun toRegisterAccount(
-        accountId: String,
-        publicIdentityKey: String,
-        isLimited: Boolean,
-        appDomain: String?
-    ): RegisteredAccount = RegisteredAccount(AccountId(accountId), PublicKey(publicIdentityKey), isLimited, appDomain)
+        accountId: String, publicIdentityKey: String, isLimited: Boolean, appDomain: String?, notifyServerWatchTopic: String?, notifyServerAuthenticationKey: String?,
+    ): RegisteredAccount =
+        RegisteredAccount(AccountId(accountId), PublicKey(publicIdentityKey), isLimited, appDomain, notifyServerWatchTopic?.let { Topic(it) }, notifyServerAuthenticationKey?.let { PublicKey(it) })
 }
