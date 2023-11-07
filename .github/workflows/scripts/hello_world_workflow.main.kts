8df184874164cb8e620a8e430dedafe3f49a006f
Talha Ali <>
2023-11-06 19:34:06 -0600

    re-added consistency job. Added second print

--- .github/workflows/scripts/hello_world_workflow.main.kts
+++ .github/workflows/scripts/hello_world_workflow.main.kts
@@ -2,6 +2,7 @@
 
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.4.0")
 
+import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
 import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
 import io.github.typesafegithub.workflows.dsl.workflow
@@ -13,7 +14,11 @@ workflow(
     sourceFile = __FILE__.toPath(),
 ) {
     job(id = "test_job", runsOn = UbuntuLatest) {
-//        uses(name = "Check out", action = CheckoutV4(ref = "feature/meta/workflow_to_notify_of_new_release"))
+        uses(
+            name = "Check out",
+            action = CheckoutV4(ref = "feature/meta/workflow_to_notify_of_new_release")
+        )
         run(name = "Print greeting", command = "echo 'Hello world!'")
+        run(name = "Print greeting", command = "echo 'Hello world!2'")
     }
 }.writeToFile()
