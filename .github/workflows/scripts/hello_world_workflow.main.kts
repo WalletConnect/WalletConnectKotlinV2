8f149606bd0670047310c461e76b5bf70eb018db
Talha Ali <>
2023-11-06 18:31:12 -0600

    removed checkout for testing

--- .github/workflows/scripts/hello_world_workflow.main.kts
+++ .github/workflows/scripts/hello_world_workflow.main.kts
@@ -2,7 +2,6 @@
 
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.4.0")
 
-import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
 import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
 import io.github.typesafegithub.workflows.dsl.workflow
@@ -14,7 +13,7 @@ workflow(
     sourceFile = __FILE__.toPath(),
 ) {
     job(id = "test_job", runsOn = UbuntuLatest) {
-        uses(name = "Check out", action = CheckoutV4())
+//        uses(name = "Check out", action = CheckoutV4(ref = "feature/meta/workflow_to_notify_of_new_release"))
         run(name = "Print greeting", command = "echo 'Hello world!'")
     }
 }.writeToFile()
