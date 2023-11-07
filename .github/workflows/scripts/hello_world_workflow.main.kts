0bb71582a9c4f1c9a92674823cf1d720114d1530
Talha Ali <>
2023-11-06 22:09:22 -0600

    updated test workflow script with slack api action

--- .github/workflows/scripts/hello_world_workflow.main.kts
+++ .github/workflows/scripts/hello_world_workflow.main.kts
@@ -4,9 +4,23 @@
 
 import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.actions.CustomAction
 import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
+import io.github.typesafegithub.workflows.dsl.expressions.Contexts
+import io.github.typesafegithub.workflows.dsl.expressions.expr
 import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.yaml.writeToFile
+import org.intellij.lang.annotations.Language
+
+@Suppress("PrivatePropertyName")
+private val SLACK_WEBHOOK_URL by Contexts.secrets
+
+@Language("JSON")
+private val slackPayload = """
+                            {
+                              "text":"Hello, World!"
+                            }
+                            """.trimIndent()
 
 workflow(
     name = "Test workflow",
@@ -18,7 +32,24 @@ workflow(
             name = "Check out",
             action = CheckoutV4(ref = "feature/meta/workflow_to_notify_of_new_release")
         )
-        run(name = "Print greeting", command = "echo 'Hello world!'")
-        run(name = "Print greeting", command = "echo 'Hello world!2'")
+        run(
+            name = "Print greeting",
+            command = "echo 'Hello world!' ${expr { SLACK_WEBHOOK_URL }} after secret"
+        )
+        uses(
+            name = "Notify Channel",
+            action = CustomAction(
+                actionOwner = "slackapi",
+                actionName = "slack-github-action",
+                actionVersion = "v1.24.0",
+                inputs = linkedMapOf(
+                    "payload" to slackPayload
+                )
+            ),
+            env = linkedMapOf(
+                "SLACK_WEBHOOK_URL" to expr { SLACK_WEBHOOK_URL },
+                "SLACK_WEBHOOK_TYPE" to "INCOMING_WEBHOOK"
+            )
+        )
     }
 }.writeToFile(addConsistencyCheck = false)
