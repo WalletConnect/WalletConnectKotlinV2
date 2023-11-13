c34dad2cd6d9c117db1f448d74897b35ce5430f0
szymon <sz.rzad@gmail.com>
2023-11-13 14:41:39 +0100

    feat: Add mixpanel to FCM events

--- sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/WalletFirebaseMessagingService.kt
+++ sample/wallet/src/main/kotlin/com/walletconnect/sample/wallet/WalletFirebaseMessagingService.kt
@@ -1,36 +1,39 @@
 package com.walletconnect.sample.wallet
 
 import android.annotation.SuppressLint
-import android.util.Log
 import com.google.firebase.messaging.RemoteMessage
+import com.walletconnect.android.internal.common.di.AndroidCommonDITags
+import com.walletconnect.android.internal.common.wcKoinApp
+import com.walletconnect.foundation.util.Logger
 import com.walletconnect.notify.client.Notify
 import com.walletconnect.notify.client.NotifyMessageService
 import com.walletconnect.sample.wallet.domain.NotificationHandler
 import kotlinx.coroutines.runBlocking
+import org.koin.core.qualifier.named
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class WalletFirebaseMessagingService : NotifyMessageService() {
-    private val TAG = this::class.simpleName
+    private val logger: Logger by lazy { wcKoinApp.koin.get(named(AndroidCommonDITags.LOGGER)) }
 
     override fun newToken(token: String) {
-        Log.d(TAG, "Registering New Token Success:\t$token")
+        logger.log("Registering New Token Success:\t$token")
     }
 
     override fun registeringFailed(token: String, throwable: Throwable) {
-        Log.d(TAG, "Registering New Token Failed:\t$token")
+        logger.log("Registering New Token Failed:\t$token")
     }
 
     override fun onMessage(message: Notify.Model.Message, originalMessage: RemoteMessage) {
-        Log.d(TAG, "Message:\t$message")
+        logger.log("Message:\t$message")
 
         runBlocking { NotificationHandler.addNotification(message) }
     }
 
     override fun onDefaultBehavior(message: RemoteMessage) {
-        Log.d(TAG, "onDefaultBehavior: ${message.to}")
+        logger.log("onDefaultBehavior: ${message.to}")
     }
 
     override fun onError(throwable: Throwable, defaultMessage: RemoteMessage) {
-        Log.e(TAG, "onError Message To: ${defaultMessage.to}", throwable)
+        logger.error("onError Message To: ${defaultMessage.to}, throwable: $throwable")
     }
 }
