1a314c28cd19fd9bb7b023002777c2f798e917ed
Talha Ali <>
2024-02-19 22:48:53 -0600

    cleaned up jacoco plugin to only look at specific dirs needed for sonar. Also removed jacoco verification task for now

--- buildSrc/src/main/kotlin/jacoco-report.gradle.kts
+++ buildSrc/src/main/kotlin/jacoco-report.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.BaseExtension
-
 plugins {
     jacoco
 }
@@ -9,102 +7,84 @@ private val fileFilter = mutableSetOf(
     "**/R\$*.class",
     "**/*\$1*", // Jacoco can not handle several "$" in class name.
     "**/BuildConfig.*",
+    "**/*\$ViewInjector*.*",
+    "**/*\$ViewBinder*.*",
     "**/Manifest*.*",
-    "**/*Test*.*",
-    "android/**/*.*",
-    "**/models/**",
+    "**/*Component.*",
+    "**/*BR.*",
+    "**/*\$InjectAdapter.*",
+    "**/*\$ModuleAdapter.*",
+    "**/*\$ViewInjector*.*",
     "**/*\$Lambda$*.*", // Jacoco can not handle several "$" in class name.
     "**/*\$inlined$*.*" // Kotlin specific, Jacoco can not handle several "$" in class name.
 )
-
-
-val javaTree = fileTree("dir" to "${project.buildDir}/intermediates/javac/debug/classes", "excludes" to fileFilter)
-val kotlinTree = fileTree("dir" to "${project.buildDir}/tmp/kotlin-classes/debug", "excludes" to fileFilter)
+private val sourceDirectoriesTree = fileTree("${project.projectDir}") {
+    include(
+        "src/main/**",
+        "src/debug/**",
+    )
+}
 private val classDirectoriesTree = fileTree("${project.buildDir}") {
     include(
-        "**/classes/**/main/**",
-        "**/intermediates/classes/debug/**",
-        "**/intermediates/javac/debug/classes/**", // Android Gradle Plugin 3.2.x support.
-        "**/tmp/kotlin-classes/debug/**"
+        "intermediates/javac/debug/classes/**",
+        "tmp/kotlin-classes/debug/**"
     )
     exclude(fileFilter)
 }
-
-private val sourceDirectoriesTree = fileTree("${project.projectDir}") {
-    include(
-        "src/main/java/**",
-        "src/main/kotlin/**",
-        "src/debug/kotlin/**",
-        "src/release/kotlin/**",
-    )
-}
-
 private val executionDataTree = fileTree(project.buildDir) {
     include(
-        "outputs/code_coverage/**/*.ec",
-        "jacoco/jacocoTestReportDebug.exec",
-        "jacoco/testDebugUnitTest.exec",
-        "jacoco/test.exec"
+        "jacoco/testDebugUnitTest.exec"
     )
-}
-
-fun JacocoReportsContainer.reports() {
-    xml.required.set(false)
-    html.required.set(true)
-    html.outputLocation.set(file("${buildDir}/reports/jacoco/html"))
-}
-
-fun JacocoCoverageVerification.setDirectories() {
-    sourceDirectories.setFrom(sourceDirectoriesTree)
-    classDirectories.setFrom(classDirectoriesTree)
-    executionData.setFrom(executionDataTree)
-}
-
-fun JacocoReport.setDirectories() {
-    sourceDirectories.setFrom(sourceDirectoriesTree)
-    classDirectories.setFrom(classDirectoriesTree)
-    executionData.setFrom(executionDataTree)
 }
 
 tasks {
-    register<JacocoReport>("jacocoAndroidTestReport") {
-        group = "Verification"
+    register<JacocoReport>("jacocoDebugTestReport") {
+        group = "Reporting"
         description = "Code coverage report for both Android and Unit tests."
-        dependsOn("testDebugUnitTest"/*, "createDebugUnitTestCoverageReport"*/)
+
+        dependsOn("testDebugUnitTest"/*, "createDebugUnitTestCoverageReport"*//*, "createDebugCoverageReport"*/)
         reports {
-            reports()
+            xml.required.set(true)
+            html.required.set(true)
+            html.outputLocation.set(file("${buildDir}/reports/jacoco/html"))
+            xml.outputLocation.set(file("${buildDir}/reports/jacoco/xml/jacoco.xml"))
         }
-        setDirectories()
+        sourceDirectories.setFrom(sourceDirectoriesTree)
+        classDirectories.setFrom(files(classDirectoriesTree))
+        executionData.setFrom(executionDataTree)
     }
 
-    register<JacocoCoverageVerification>("jacocoAndroidCoverageVerification") {
-        group = "Verification"
-        description = "Code coverage verification for Android both Android and Unit tests."
-        dependsOn("testDebugUnitTest", "jacocoAndroidTestReport"/*, "createDebugUnitTestCoverageReport"*/)
-        violationRules {
-            rule {
-                element = "CLASS"
-                excludes = listOf(
-                    "**.FactorFacade.Builder",
-                    "**.ServiceFacade.Builder",
-                    "**.ChallengeFacade.Builder",
-                    "**.Task"
-                )
-                limit {
-                    minimum = "0.0".toBigDecimal()
-                }
-            }
-        }
-        setDirectories()
-    }
-}
-
-(project as? ExtensionAware)?.extensions?.configure<BaseExtension>("android") {
-    buildTypes.forEach { builtType ->
-        builtType.enableUnitTestCoverage = false
-    }
+//    register<JacocoCoverageVerification>("jacocoAndroidCoverageVerification") {
+//        group = "Verification"
+//        description = "Code coverage verification for Android both Android and Unit tests."
+//
+////        if (project.tasks.contains(task("compileDebugLibraryResources"))) {
+////            dependsOn("compileDebugLibraryResources")
+////        }
+//
+//        dependsOn("testDebugUnitTest", "jacocoAndroidTestReport"/*, "createDebugUnitTestCoverageReport"*/)
+//
+//        violationRules {
+//            rule {
+//                element = "CLASS"
+//                excludes = listOf(
+//                    "**.FactorFacade.Builder",
+//                    "**.ServiceFacade.Builder",
+//                    "**.ChallengeFacade.Builder",
+//                    "**.Task"
+//                )
+//                limit {
+//                    minimum = "0.0".toBigDecimal()
+//                }
+//            }
+//        }
+//        sourceDirectories.setFrom(sourceDirectoriesTree)
+//        classDirectories.setFrom(classDirectoriesTree)
+//        executionData.setFrom(executionDataTree)
+//    }
 }
 
 afterEvaluate {
-    project.tasks.getByName("testDebugUnitTest").finalizedBy("jacocoAndroidTestReport", "jacocoAndroidCoverageVerification")
+    project.tasks.getByName("testDebugUnitTest")
+        .finalizedBy("jacocoDebugTestReport"/*, "jacocoAndroidCoverageVerification"*/)
 }
