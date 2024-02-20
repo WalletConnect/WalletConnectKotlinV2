f1e53b1d5910cb1e2a3b78202e446ca77bf574d2
Talha Ali <>
2024-02-20 08:23:50 -0600

    added VERIFY_URL from FailoverUtil file

--- core/android/src/main/kotlin/com/walletconnect/android/internal/common/di/VerifyModule.kt
+++ core/android/src/main/kotlin/com/walletconnect/android/internal/common/di/VerifyModule.kt
@@ -7,9 +7,12 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
+private var VERIFY_URL: String = "https://verify.walletconnect.com/"
+
 @JvmSynthetic
 internal fun verifyModule() = module {
-    factory(named(AndroidCommonDITags.VERIFY_URL)) { VERIFY_URL }
+
+factory(named(AndroidCommonDITags.VERIFY_URL)) { VERIFY_URL }
 
     single(named(AndroidCommonDITags.VERIFY_RETROFIT)) {
         Retrofit.Builder()
