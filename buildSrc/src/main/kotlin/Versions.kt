1899e6640f694f71df8562f34fd6e73140bfc927
kubel <jakubwitczak6@gmail.com>
2024-03-25 10:53:04 +0100

    Version bump

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,17 +5,17 @@ const val KEY_PUBLISH_ARTIFACT_ID = "PUBLISH_ARTIFACT_ID"
 const val KEY_SDK_NAME = "SDK_NAME"
 
 //Latest versions
-const val BOM_VERSION = "1.30.0"
+const val BOM_VERSION = "1.31.0"
 const val FOUNDATION_VERSION = "1.16.0"
-const val CORE_VERSION = "1.30.0"
-const val SIGN_VERSION = "2.30.0"
-const val AUTH_VERSION = "1.27.0"
-const val CHAT_VERSION = "1.0.0-beta27"
-const val NOTIFY_VERSION = "1.2.0"
-const val WEB_3_WALLET_VERSION = "1.30.0"
-const val WEB_3_MODAL_VERSION = "1.4.0"
-const val WC_MODAL_VERSION = "1.4.0"
-const val MODAL_CORE_VERSION = "1.4.0"
+const val CORE_VERSION = "1.31.0"
+const val SIGN_VERSION = "2.31.0"
+const val AUTH_VERSION = "1.28.0"
+const val CHAT_VERSION = "1.0.0-beta28"
+const val NOTIFY_VERSION = "1.3.0"
+const val WEB_3_WALLET_VERSION = "1.31.0"
+const val WEB_3_MODAL_VERSION = "1.5.0"
+const val WC_MODAL_VERSION = "1.5.0"
+const val MODAL_CORE_VERSION = "1.5.0"
 
 val jvmVersion = JavaVersion.VERSION_11
 const val MIN_SDK: Int = 23
