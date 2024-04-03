c59773d9e411f9e876dc997ff6e6e5ed8dd8d4c5
kubel <jakubwitczak6@gmail.com>
2024-04-03 17:08:15 +0200

    Add confirm email screen

--- product/web3modal/src/main/kotlin/com/walletconnect/web3/modal/ui/routes/connect/email/ConfirmEmail.kt
+++ product/web3modal/src/main/kotlin/com/walletconnect/web3/modal/ui/routes/connect/email/ConfirmEmail.kt
@@ -1,6 +1,21 @@
 package com.walletconnect.web3.modal.ui.routes.connect.email
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.walletconnect.web3.modal.ui.components.internal.commons.HorizontalSpacer
+import com.walletconnect.web3.modal.ui.components.internal.commons.VerticalSpacer
+import com.walletconnect.web3.modal.ui.components.internal.email.code.CodeInput
+import com.walletconnect.web3.modal.ui.components.internal.email.code.rememberCodeInputState
+import com.walletconnect.web3.modal.ui.previews.UiModePreview
+import com.walletconnect.web3.modal.ui.theme.Web3ModalTheme
 
 @Composable
 internal fun ConfirmEmailRoute() {
@@ -9,5 +24,56 @@ internal fun ConfirmEmailRoute() {
 
 @Composable
 private fun ConfirmEmail() {
+    //TODO: Add loading state
+    val codeInputState = rememberCodeInputState { code ->
+        println("kobe: code")
+    }
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        VerticalSpacer(16.dp)
+        Text(
+            text = "Enter the code we sent to",
+            style = Web3ModalTheme.typo.paragraph400,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "jakub@walletconnect.com", //TODO: Replace with email
+            style = Web3ModalTheme.typo.paragraph400,
+            textAlign = TextAlign.Center
+        )
+        VerticalSpacer(12.dp)
+        Text(
+            text = "The code expires in 10 minutes",
+            style = Web3ModalTheme.typo.small400.copy(Web3ModalTheme.colors.foreground.color200),
+            textAlign = TextAlign.Center
+        )
+        VerticalSpacer(16.dp)
+        CodeInput(codeInputState)
+        VerticalSpacer(12.dp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Didn't receive it?",
+                style = Web3ModalTheme.typo.paragraph400
+            )
+            HorizontalSpacer(width = 4.dp)
+            Text(
+                text = "Resend code",
+                style = Web3ModalTheme.typo.small600.copy(color = Web3ModalTheme.colors.accent100)
+            ) //TODO: Implement resend link
+        }
+        VerticalSpacer(20.dp)
+    }
+}
+
+@Composable
+@UiModePreview
+private fun ConfirmEmailPreview() {
+    ConfirmEmail()
 }
