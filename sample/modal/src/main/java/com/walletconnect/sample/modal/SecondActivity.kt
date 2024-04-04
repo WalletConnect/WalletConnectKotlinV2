6bd318ac3ca7e7817937fc984726432489837229
kubel <jakubwitczak6@gmail.com>
2024-04-04 17:20:11 +0200

    Add comment

--- sample/modal/src/main/java/com/walletconnect/sample/modal/SecondActivity.kt
+++ sample/modal/src/main/java/com/walletconnect/sample/modal/SecondActivity.kt
@@ -2,7 +2,7 @@ package com.walletconnect.sample.modal
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
+//note: only for testing, remove me later
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
