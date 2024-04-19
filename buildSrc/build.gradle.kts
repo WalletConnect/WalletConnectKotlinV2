bc4533b06c5b38198e3e65d999bf11c8a67b45d3
Talha Ali <Talhaali00@gmail.com>
2024-04-19 11:24:26 -0500

    removed comments, version catalog manages versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}") // when changing, remember to change version in Versions.kt in buildSrc module
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}") // when changing, remember to change version in Versions.kt in buildSrc module
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${libs.versions.dokka.get()}") // when changing, remember to change version in Versions.kt in buildSrc module
-    implementation("com.google.firebase:firebase-appdistribution-gradle:${libs.versions.firebaseAppDistribution.get()}") // when changing, remember to change version in root build.gradle.kts
+    implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${libs.versions.dokka.get()}")
+    implementation("com.google.firebase:firebase-appdistribution-gradle:${libs.versions.firebaseAppDistribution.get()}")
 
-    testImplementation("junit:junit:${libs.versions.jUnit.get()}") // when changing, remember to change version in Versions.kt in buildSrc module
+    testImplementation("junit:junit:${libs.versions.jUnit.get()}")
 }
