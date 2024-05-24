a4b88ffba97f37655744374e7796180d093c4a6a
kubel <jakubwitczak6@gmail.com>
2024-05-24 07:27:40 +0200

    Remove comments

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 }
 
 dependencies {
-//    Gradle plugin
     implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:${libs.versions.dokka.get()}")
