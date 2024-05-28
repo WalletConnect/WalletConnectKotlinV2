1fea907d12ac40dae8a0fa1311e9b030ade32e0a
kubel <jakubwitczak6@gmail.com>
2024-05-28 10:38:41 +0200

    Add test cases to SendEventUseCaseTest.kt

--- core/android/src/test/kotlin/com/walletconnect/android/internal/SendEventUseCaseTest.kt
+++ core/android/src/test/kotlin/com/walletconnect/android/internal/SendEventUseCaseTest.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -76,5 +77,66 @@ class SendEventUseCaseTest : KoinTest {
 
         coVerify { pulseService.sendEvent(body = event, sdkType = sdkType.type) }
         verify { logger.log("Event sent successfully: ${event.props.type}") }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `send should log error when analytics is enabled and response is unsuccessful`() = runTest(testDispatcher) {
+        val props = Props(type = "testEvent")
+        val sdkType = SDKType.WEB3MODAL
+        val event = Event(props = props, bundleId = bundleId)
+
+        coEvery { pulseService.sendEvent(any(), any()) } returns Response.error(400, "ResponseBody".toResponseBody())
+        every { logger.error("Failed to send event: ${event.props.type}") } just Runs
+
+        useCase.send(props, sdkType, event.timestamp, event.eventId)
+
+        advanceUntilIdle()
+
+        coVerify { pulseService.sendEvent(body = event, sdkType = sdkType.type) }
+        verify { logger.error("Failed to send event: ${event.props.type}") }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `send should log exception when analytics is enabled and an exception occurs`() = runTest(testDispatcher) {
+        val props = Props(type = "testEvent")
+        val sdkType = SDKType.WEB3MODAL
+        val event = Event(props = props, bundleId = bundleId)
+        val exception = Exception("Test exception")
+
+        coEvery { pulseService.sendEvent(any(), any()) } throws exception
+        every { logger.error("Failed to send event: ${props.type}, error: $exception") } just Runs
+
+        useCase.send(props, sdkType, event.timestamp, event.eventId)
+
+        advanceUntilIdle()
+
+        coVerify { pulseService.sendEvent(body = event, sdkType = sdkType.type) }
+        verify { logger.error("Failed to send event: ${props.type}, error: $exception") }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `send should not send event when analytics is disabled`() = runTest(testDispatcher) {
+        stopKoin()
+        val module: Module = module {
+            single(named(AndroidCommonDITags.ENABLE_WEB_3_MODAL_ANALYTICS)) { false }
+        }
+        val app2 = startKoin { modules(module) }
+        useCase = SendEventUseCase(pulseService, logger, bundleId)
+        wcKoinApp = app2
+
+        val props = Props(type = "testEvent")
+        val sdkType = SDKType.WEB3MODAL
+        val event = Event(props = props, bundleId = bundleId)
+
+        useCase.send(props, sdkType, event.timestamp, event.eventId)
+
+        advanceUntilIdle()
+
+        coVerify(exactly = 0) { pulseService.sendEvent(any(), any()) }
+        verify(exactly = 0) { logger.log("") }
+        verify(exactly = 0) { logger.error("") }
     }
 }
