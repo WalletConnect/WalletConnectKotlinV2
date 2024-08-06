fc6841820115401ba70b6ec8efec8a45ca50a24f
kubel <jakubwitczak6@gmail.com>
2024-08-06 16:30:29 +0200

    Clean up

--- core/android/src/main/kotlin/com/walletconnect/android/internal/common/jwt/clientid/GetClientIdUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.walletconnect.android.internal.common.jwt.clientid
-
-import android.content.SharedPreferences
-import com.walletconnect.android.internal.common.di.KEY_CLIENT_ID
-
-class GetClientIdUseCase(private val sharedPreferences: SharedPreferences) {
-    operator fun invoke(): String = sharedPreferences.getString(KEY_CLIENT_ID, null) ?: throw IllegalStateException("Client ID not found")
-}
